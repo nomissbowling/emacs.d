52d45c50ae22cdf54a8043fbb907e02ff80b848f
minorugh <minorugh@gmail.com>
2020-01-28 17:16:59 +0900

    Delete ediff setting

--- inits/05_misc.el
+++ inits/05_misc.el
@@ -38,13 +38,6 @@
 (persistent-scratch-setup-default)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ediff
-
-(setq ediff-window-setup-function 'ediff-setup-windows-plain
-      ediff-split-window-function 'split-window-horizontally
-      ediff-diff-options "-twB")
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; quickrun
 
 (bind-key [f5] 'quickrun)

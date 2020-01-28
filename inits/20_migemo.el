95feabb088e674b55e9df7512450f681551dcdf8
minorugh <minorugh@gmail.com>
2020-01-28 17:30:32 +0900

    Delete setting of darwin

--- inits/20_migemo.el
+++ inits/20_migemo.el
@@ -15,12 +15,8 @@
   (migemo-coding-system 'utf-8-unix)
   :config
   ;; migemo-dict installed path
-  (when (eq system-type 'darwin)
-    (setq migemo-command "/usr/local/bin/cmigemo"
-	  migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict"))
-  (when (eq system-type 'gnu/linux)
-    (setq migemo-command "/usr/bin/cmigemo"
-	  migemo-dictionary "/usr/share/cmigemo/utf-8/migemo-dict")))
+  (setq migemo-command "/usr/bin/cmigemo"
+	migemo-dictionary "/usr/share/cmigemo/utf-8/migemo-dict"))
 
 ;; Local Variables:
 ;; no-byte-compile: t

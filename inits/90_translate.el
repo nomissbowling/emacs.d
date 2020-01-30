5dd4f73528a1899fc216fe7e66923d18e21ee367
minorugh <minorugh@gmail.com>
2020-01-30 16:24:18 +0900

    Fix

--- inits/90_translate.el
+++ inits/90_translate.el
@@ -33,13 +33,11 @@
 	   "ja" "en"
 	   string))))))
 
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Fix error of "Failed to search TKK"
 (defun google-translate--get-b-d1 ()
   "TKK='427110.1469889687'."
   (list 427110 1469889687))
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Fix error of "args-out-of-range"
@@ -54,7 +52,6 @@
 ;;     (if (and info (> (length info) 0))
 ;;         (aref info 1)
 ;;       nil)))
-
 
 ;; Local Variables:
 ;; no-byte-compile: t

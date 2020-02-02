6eeb8e21105acd929bd4d555cbcf2a7c4c64aae0
minorugh <minorugh@gmail.com>
2020-02-02 18:52:25 +0900

    Fix key-chord bind for hydra-pinky

--- inits/10_hydra.el
+++ inits/10_hydra.el
@@ -101,7 +101,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Hydra Pinky
 (key-chord-define-global
- "jk"
+ ".."
  (defhydra hydra-pinky (:color red :hint nil)
    "
  🐳 Pinky: _h_._l_._j_._k_._a_._e_._SPC_._b_._g_._G_._o_._w_._@_._s_._S_._/_._v_._f_._0_._1_._2_._3_._x_._<_._>_._:_"

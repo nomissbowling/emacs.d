6ad067ffc58b2c1f7a120ca8197fb3ec8c25db55
minorugh <minorugh@gmail.com>
2020-02-05 23:04:24 +0900

    Fix UI setting

--- init.el
+++ init.el
@@ -5,12 +5,12 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; UI
-(setq inhibit-splash-screen t)
-(setq inhibit-startup-message t)
 (set-frame-parameter nil 'fullscreen 'maximized)
-(scroll-bar-mode 0)
 (tool-bar-mode 0)
 (menu-bar-mode 0)
+(scroll-bar-mode 0)
+(setq inhibit-splash-screen t)
+(setq inhibit-startup-message t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Speed up startup

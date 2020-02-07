b61528a37910ec421ee1a813db3b0906b8bf4698
minorugh <minorugh@gmail.com>
2020-02-07 19:44:24 +0900

    Fix UI

--- init.el
+++ init.el
@@ -5,12 +5,12 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; UI
+(scroll-bar-mode 0)
 (tool-bar-mode 0)
 (menu-bar-mode 0)
-(scroll-bar-mode 0)
+(set-frame-parameter nil 'fullscreen 'maximized)
 (setq inhibit-splash-screen t)
 (setq inhibit-startup-message t)
-(set-frame-parameter nil 'fullscreen 'maximized)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Speed up startup

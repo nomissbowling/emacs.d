bfc1c1616049c06fb357b63ec38059c1d7958f41
minorugh <minorugh@gmail.com>
2020-02-10 09:29:10 +0900

    Delete key-chord bind

--- inits/60_howm.el
+++ inits/60_howm.el
@@ -42,7 +42,6 @@
   ("b" my:howm-blog)
   ("m" my:howm-mail)
   ("@" howm-list-all))
-(key-chord-define-global "@@" 'howm-list-all)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Set template

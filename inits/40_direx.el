01bf7f2fb0edaeb9bf8b3ad8ddd4c0689ea40018
minorugh <minorugh@gmail.com>
2020-02-13 22:53:52 +0900

    New file

---
+++ inits/40_direx.el
@@ -0,0 +1,27 @@
+;;; 40_direx.el --- 40_direx.el
+;;; Commentary:
+;;; Code:
+;; (setq debug-on-error t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq direx:leaf-icon "  " direx:open-icon "📂" direx:closed-icon "📁")
+(push '(direx:direx-mode :position left :width 30 :dedicated t)
+      popwin:special-display-config)
+
+;; use direx-project.el
+;; https://blog.shibayu36.org/entry/2013/02/12/191459
+(bind-key
+ [f11]
+ (defun direx:jump-to-project-directory ()
+   "If in project, launch direx-project otherwise start direx."
+   (interactive)
+   (let ((result (ignore-errors
+		   (direx-project:jump-to-project-root-other-window)
+		   t)))
+     (unless result
+       (direx:jump-to-directory-other-window)))))
+
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
+;;; 40_direx.el ends here

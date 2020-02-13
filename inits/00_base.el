1b7d72f347e5e873a316128d80acca52ac4bf3df
minorugh <minorugh@gmail.com>
2020-02-13 20:47:16 +0900

    Fix auto-mode-alist

--- inits/00_base.el
+++ inits/00_base.el
@@ -100,14 +100,23 @@
 (setq scroll-preserve-screen-position t)
 
 ;; Set major mode by extension
-(add-to-list 'auto-mode-alist '("\\diary.txt?\\|\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mak\\'" . makefile-mode))
 
 ;; Copy with mouse drag
 (setq mouse-drag-copy-region t)
 
-;; Use clipboard
-(setq x-select-enable-clipboard t)
+;; Use the X11 clipboard
+(setq select-enable-clipboard  t)
+(bind-key "M-w" 'clipboard-kill-ring-save)
+(bind-key "C-w" 'my/clipboard-kill-region)
+(defun my/clipboard-kill-region ()
+  "If the region is active, `clipboard-kill-region'.
+If the region is inactive, `backward-kill-word'."
+  (interactive)
+  (if (use-region-p)
+      (clipboard-kill-region (region-beginning) (region-end))
+    (backward-kill-word 1)))
 
 ;; Exit Emacs with M-x exit
 (defalias 'exit 'save-buffers-kill-emacs)

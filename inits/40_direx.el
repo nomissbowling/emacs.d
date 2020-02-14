5674ff1fb29d4d67f76f7c61adf49c19e8f31328
minorugh <minorugh@gmail.com>
2020-02-14 10:43:06 +0900

    Fix

--- inits/40_direx.el
+++ inits/40_direx.el
@@ -5,9 +5,10 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Use direx.el
-(setq direx:leaf-icon "  " direx:open-icon "📂" direx:closed-icon "📁")
-(push '(direx:direx-mode :position left :width 30 :dedicated t)
-      popwin:special-display-config)
+(use-package direx
+  :config
+  (setq direx:leaf-icon "  " direx:open-icon "📂" direx:closed-icon "📁")
+  (push '(direx:direx-mode :position left :width 30 :dedicated t) popwin:special-display-config))
 
 ;; Use direx-project.el
 ;; https://blog.shibayu36.org/entry/2013/02/12/191459

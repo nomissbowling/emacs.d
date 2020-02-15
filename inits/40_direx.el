3cdc551b825d2890fdeb407689a1e1419c17b906
minorugh <minorugh@gmail.com>
2020-02-15 17:59:42 +0900

    Fix key bind

--- inits/40_direx.el
+++ inits/40_direx.el
@@ -4,24 +4,22 @@
 ;; (setq debug-on-error t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Use direx.el
 (use-package direx
+  :bind (([f11] . direx:jump-to-project-directory)
+	 :map direx:direx-mode-map
+	 ([f11] . quit-window))
   :config
   (setq direx:leaf-icon "  " direx:open-icon "📂" direx:closed-icon "📁")
-  (push '(direx:direx-mode :position left :width 30 :dedicated t) popwin:special-display-config))
-
-;; Use direx-project.el
-;; https://blog.shibayu36.org/entry/2013/02/12/191459
-(bind-key
- [f11]
- (defun direx:jump-to-project-directory ()
-   "If in project, launch direx-project otherwise start direx."
-   (interactive)
-   (let ((result (ignore-errors
-		   (direx-project:jump-to-project-root-other-window)
-		   t)))
-     (unless result
-       (direx:jump-to-directory-other-window)))))
+  (push '(direx:direx-mode :position left :width 25 :dedicated t) popwin:special-display-config)
+  ;; https://blog.shibayu36.org/entry/2013/02/12/191459
+  (defun direx:jump-to-project-directory ()
+    "If in project, launch direx-project otherwise start direx."
+    (interactive)
+    (let ((result (ignore-errors
+		    (direx-project:jump-to-project-root-other-window)
+		    t)))
+      (unless result
+	(direx:jump-to-directory-other-window)))))
 
 ;; Local Variables:
 ;; no-byte-compile: t

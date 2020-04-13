e82e0f7af4d5e9e578578c854a1dc2809e82cec1
minorugh <minorugh@gmail.com>
2020-04-13 19:52:00 +0900

    Delete my:dired-ediff

--- inits/40_dired.el
+++ inits/40_dired.el
@@ -98,27 +98,6 @@
     (when (getenv "WSLENV")
       (call-process "wslstart" nil 0 nil fn))))
 
-(defun my:dired-ediff-files ()
-  "Start ediff with a file marked with `dired-mode'."
-  (interactive)
-  (let ((files (dired-get-marked-files))
-        (wnd (current-window-configuration)))
-    (if (<= (length files) 2)
-        (let ((file1 (car files))
-	      (file2 (if (cdr files)
-                         (cadr files)
-		       (read-file-name
-                        "file: "
-                        (dired-dwim-target-directory)))))
-          (if (file-newer-than-file-p file1 file2)
-	      (ediff-files file2 file1)
-            (ediff-files file1 file2))
-          (add-hook 'ediff-after-quit-hook-internal
-                    (lambda ()
-		      (setq ediff-after-quit-hook-internal nil)
-		      (set-window-configuration wnd))))
-      (error "No more than 2 files should be marked"))))
-
 (defun my:dired-toggle-mark (arg)
   "Toggle the current (or next ARG) files."
   (interactive "p")
@@ -137,67 +116,66 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Shows icons
 ;; Tips from https://github.com/seagle0128/.emacs.d/blob/master/lisp/init-dired.el
-(when (eq system-type 'gnu/linux)
-  (leaf all-the-icons-dired
-    :commands dired-mode
-    :hook
-    (dired-mode-hook . all-the-icons-dired-mode)
-    :custom
-    ;; Dired files deleted by trash and no ask recursive
-    (delete-by-moving-to-trash . t)
-    (dired-recursive-copies . 'always)
-    (dired-recursive-deletes . 'always)
-    ;; When dired opened in two windows, move or copy in the other dired
-    (dired-dwim-target . t)
-    ;; Recursively copy directory
-    (dired-recursive-copies . 'always)
-    :config
-    (defun my:all-the-icons-dired--display ()
-      "Display the icons of files without colors in a dired buffer."
-      (when dired-subdir-alist
-	(let ((inhibit-read-only t)
-	      (remote-p (and (fboundp 'tramp-tramp-file-p)
-			     (tramp-tramp-file-p default-directory))))
-	  (save-excursion
-	    ;; TRICK: Use TAB to align icons
-	    (setq-local tab-width 1)
-	    (goto-char (point-min))
-	    (while (not (eobp))
-	      (when (dired-move-to-filename nil)
-		(insert " ")
-		(let ((file (dired-get-filename 'verbatim t)))
-		  (unless (member file '("." ".."))
-		    (let ((filename (file-local-name (dired-get-filename nil t))))
-		      (if (file-directory-p filename)
-			  (let ((icon (cond
-				       (remote-p
-					(all-the-icons-octicon "file-directory"
-							       :v-adjust all-the-icons-dired-v-adjust
-							       :face 'all-the-icons-dired-dir-face))
-				       ((file-symlink-p filename)
-					(all-the-icons-octicon "file-symlink-directory"
-							       :v-adjust all-the-icons-dired-v-adjust
-							       :face 'all-the-icons-dired-dir-face))
-				       ((all-the-icons-dir-is-submodule filename)
-					(all-the-icons-octicon "file-submodule"
-							       :v-adjust all-the-icons-dired-v-adjust
-							       :face 'all-the-icons-dired-dir-face))
-				       ((file-exists-p (format "%s/.git" filename))
-					(all-the-icons-octicon "repo"
-							       :height 1.1
-							       :v-adjust all-the-icons-dired-v-adjust
-							       :face 'all-the-icons-dired-dir-face))
-				       (t (let ((matcher (all-the-icons-match-to-alist
-							  file all-the-icons-dir-icon-alist)))
-					    (apply (car matcher)
-						   (list (cadr matcher)
-							 :face 'all-the-icons-dired-dir-face
-							 :v-adjust all-the-icons-dired-v-adjust)))))))
-			    (insert icon))
-			(insert (all-the-icons-icon-for-file file :v-adjust all-the-icons-dired-v-adjust))))
-		    (insert "\t"))))   ; Align and keep one space for refeshing after operations
-	      (forward-line 1))))))
-    (advice-add #'all-the-icons-dired--display :override #'my:all-the-icons-dired--display)))
+(leaf all-the-icons-dired
+  :commands dired-mode
+  :hook
+  (dired-mode-hook . all-the-icons-dired-mode)
+  :custom
+  ;; Dired files deleted by trash and no ask recursive
+  (delete-by-moving-to-trash . t)
+  (dired-recursive-copies . 'always)
+  (dired-recursive-deletes . 'always)
+  ;; When dired opened in two windows, move or copy in the other dired
+  (dired-dwim-target . t)
+  ;; Recursively copy directory
+  (dired-recursive-copies . 'always)
+  :config
+  (defun my:all-the-icons-dired--display ()
+    "Display the icons of files without colors in a dired buffer."
+    (when dired-subdir-alist
+      (let ((inhibit-read-only t)
+	    (remote-p (and (fboundp 'tramp-tramp-file-p)
+			   (tramp-tramp-file-p default-directory))))
+	(save-excursion
+	  ;; TRICK: Use TAB to align icons
+	  (setq-local tab-width 1)
+	  (goto-char (point-min))
+	  (while (not (eobp))
+	    (when (dired-move-to-filename nil)
+	      (insert " ")
+	      (let ((file (dired-get-filename 'verbatim t)))
+		(unless (member file '("." ".."))
+		  (let ((filename (file-local-name (dired-get-filename nil t))))
+		    (if (file-directory-p filename)
+			(let ((icon (cond
+				     (remote-p
+				      (all-the-icons-octicon "file-directory"
+							     :v-adjust all-the-icons-dired-v-adjust
+							     :face 'all-the-icons-dired-dir-face))
+				     ((file-symlink-p filename)
+				      (all-the-icons-octicon "file-symlink-directory"
+							     :v-adjust all-the-icons-dired-v-adjust
+							     :face 'all-the-icons-dired-dir-face))
+				     ((all-the-icons-dir-is-submodule filename)
+				      (all-the-icons-octicon "file-submodule"
+							     :v-adjust all-the-icons-dired-v-adjust
+							     :face 'all-the-icons-dired-dir-face))
+				     ((file-exists-p (format "%s/.git" filename))
+				      (all-the-icons-octicon "repo"
+							     :height 1.1
+							     :v-adjust all-the-icons-dired-v-adjust
+							     :face 'all-the-icons-dired-dir-face))
+				     (t (let ((matcher (all-the-icons-match-to-alist
+							file all-the-icons-dir-icon-alist)))
+					  (apply (car matcher)
+						 (list (cadr matcher)
+						       :face 'all-the-icons-dired-dir-face
+						       :v-adjust all-the-icons-dired-v-adjust)))))))
+			  (insert icon))
+		      (insert (all-the-icons-icon-for-file file :v-adjust all-the-icons-dired-v-adjust))))
+		  (insert "\t"))))   ; Align and keep one space for refeshing after operations
+	    (forward-line 1))))))
+  (advice-add #'all-the-icons-dired--display :override #'my:all-the-icons-dired--display))
 
 ;; Local Variables:
 ;; no-byte-compile: t

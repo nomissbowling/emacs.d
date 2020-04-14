4078246aa9ca2cbf6c6ad7cd4701dfde16bbe517
minorugh <minorugh@gmail.com>
2020-04-14 21:51:26 +0900

    Fix comment

--- inits/90_translate.el
+++ inits/90_translate.el
@@ -35,22 +35,24 @@
   ;; Fix error of "Failed to search TKK"
   (defun google-translate--get-b-d1 ()
     "TKK='427110.1469889687'."
-    (list 427110 1469889687)))
+    (list 427110 1469889687))
 
-;; Fix error of "args-out-of-range"
-;; Replace function in google-translate-core.el
-
-;; (defun google-translate-json-suggestion (json)
-;;   "Retrieve from JSON (which returns by the
-;; `google-translate-request' function) suggestion. This function
-;; does matter when translating misspelled word. So instead of
-;; translation it is possible to get suggestion."
-;;   (let ((info (aref json 7)))
-;; -  (when info
-;; -  (aref info 1))))
-;; +    (when (and info (> (length info) 0))
-;; +        (aref info 1)
-;; +      nil)))
+  ;; Fix error of `args-out-of-range'
+  ;; ---------------------------------------------------------------
+  ;; Replace function in google-translate-core.el
+  ;; (defun google-translate-json-suggestion (json)
+  ;;   "Retrieve from JSON (which returns by the
+  ;; `google-translate-request' function) suggestion. This function
+  ;; does matter when translating misspelled word. So instead of
+  ;; translation it is possible to get suggestion."
+  ;;   (let ((info (aref json 7)))
+  ;; -  (when info
+  ;; -  (aref info 1))))
+  ;; +    (when (and info (> (length info) 0))
+  ;; +        (aref info 1)
+  ;; +      nil)))
+  ;; -----------------------------------------------------------------
+  )
 
 ;; Local Variables:
 ;; no-byte-compile: t

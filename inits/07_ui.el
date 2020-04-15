063fdaede7e8d4669aadeaa163e38cad45e5823f
minorugh <minorugh@gmail.com>
2020-04-15 20:49:09 +0900

    Fix leaf setting

--- inits/07_ui.el
+++ inits/07_ui.el
@@ -53,69 +53,70 @@
     (display-line-numbers-mode 1)))
 
 (leaf all-the-icons
+  :custom
+  (all-the-icons-scale-factor . 1.0))
+
+(leaf all-the-icons-dired
+  :url "https://github.com/seagle0128/.emacs.d/blob/master/lisp/init-dired.el"
+  :commands dired-mode
+  :hook
+  (dired-mode-hook . all-the-icons-dired-mode)
+  :custom
+  ;; Dired files deleted by trash and no ask recursive
+  (delete-by-moving-to-trash . t)
+  (dired-recursive-copies . :always)
+  (dired-recursive-deletes . :always)
+  ;; When dired opened in two windows, move or copy in the other dired
+  (dired-dwim-target . t)
+  ;; Recursively copy directory
+  (dired-recursive-copies . :always)
   :config
-  (setq all-the-icons-scale-factor 1.0)
-  (leaf all-the-icons-dired
-    :url "https://github.com/seagle0128/.emacs.d/blob/master/lisp/init-dired.el"
-    :commands dired-mode
-    :hook
-    (dired-mode-hook . all-the-icons-dired-mode)
-    :custom
-    ;; Dired files deleted by trash and no ask recursive
-    (delete-by-moving-to-trash . t)
-    (dired-recursive-copies . 'always)
-    (dired-recursive-deletes . 'always)
-    ;; When dired opened in two windows, move or copy in the other dired
-    (dired-dwim-target . t)
-    ;; Recursively copy directory
-    (dired-recursive-copies . 'always)
-    :config
-    (defun my:all-the-icons-dired--display ()
-      "Display the icons of files without colors in a dired buffer."
-      (when dired-subdir-alist
-	(let ((inhibit-read-only t)
-	      (remote-p (and (fboundp 'tramp-tramp-file-p)
-			     (tramp-tramp-file-p default-directory))))
-	  (save-excursion
-	    ;; TRICK: Use TAB to align icons
-	    (setq-local tab-width 1)
-	    (goto-char (point-min))
-	    (while (not (eobp))
-	      (when (dired-move-to-filename nil)
-		(insert " ")
-		(let ((file (dired-get-filename 'verbatim t)))
-		  (unless (member file '("." ".."))
-		    (let ((filename (file-local-name (dired-get-filename nil t))))
-		      (if (file-directory-p filename)
-			  (let ((icon (cond
-				       (remote-p
-					(all-the-icons-octicon "file-directory"
-							       :v-adjust all-the-icons-dired-v-adjust
-							       :face 'all-the-icons-dired-dir-face))
-				       ((file-symlink-p filename)
-					(all-the-icons-octicon "file-symlink-directory"
-							       :v-adjust all-the-icons-dired-v-adjust
-							       :face 'all-the-icons-dired-dir-face))
-				       ((all-the-icons-dir-is-submodule filename)
-					(all-the-icons-octicon "file-submodule"
-							       :v-adjust all-the-icons-dired-v-adjust
-							       :face 'all-the-icons-dired-dir-face))
-				       ((file-exists-p (format "%s/.git" filename))
-					(all-the-icons-octicon "repo"
-							       :height 1.1
-							       :v-adjust all-the-icons-dired-v-adjust
-							       :face 'all-the-icons-dired-dir-face))
-				       (t (let ((matcher (all-the-icons-match-to-alist
-							  file all-the-icons-dir-icon-alist)))
-					    (apply (car matcher)
-						   (list (cadr matcher)
-							 :face 'all-the-icons-dired-dir-face
-							 :v-adjust all-the-icons-dired-v-adjust)))))))
-			    (insert icon))
-			(insert (all-the-icons-icon-for-file file :v-adjust all-the-icons-dired-v-adjust))))
-		    (insert "\t"))))   ; Align and keep one space for refeshing after operations
-	      (forward-line 1))))))
-    (advice-add #'all-the-icons-dired--display :override #'my:all-the-icons-dired--display)))
+  (defun my:all-the-icons-dired--display ()
+    "Display the icons of files without colors in a dired buffer."
+    (when dired-subdir-alist
+      (let ((inhibit-read-only t)
+	    (remote-p (and (fboundp 'tramp-tramp-file-p)
+			   (tramp-tramp-file-p default-directory))))
+	(save-excursion
+	  ;; TRICK: Use TAB to align icons
+	  (setq-local tab-width 1)
+	  (goto-char (point-min))
+	  (while (not (eobp))
+	    (when (dired-move-to-filename nil)
+	      (insert " ")
+	      (let ((file (dired-get-filename 'verbatim t)))
+		(unless (member file '("." ".."))
+		  (let ((filename (file-local-name (dired-get-filename nil t))))
+		    (if (file-directory-p filename)
+			(let ((icon (cond
+				     (remote-p
+				      (all-the-icons-octicon "file-directory"
+							     :v-adjust all-the-icons-dired-v-adjust
+							     :face 'all-the-icons-dired-dir-face))
+				     ((file-symlink-p filename)
+				      (all-the-icons-octicon "file-symlink-directory"
+							     :v-adjust all-the-icons-dired-v-adjust
+							     :face 'all-the-icons-dired-dir-face))
+				     ((all-the-icons-dir-is-submodule filename)
+				      (all-the-icons-octicon "file-submodule"
+							     :v-adjust all-the-icons-dired-v-adjust
+							     :face 'all-the-icons-dired-dir-face))
+				     ((file-exists-p (format "%s/.git" filename))
+				      (all-the-icons-octicon "repo"
+							     :height 1.1
+							     :v-adjust all-the-icons-dired-v-adjust
+							     :face 'all-the-icons-dired-dir-face))
+				     (t (let ((matcher (all-the-icons-match-to-alist
+							file all-the-icons-dir-icon-alist)))
+					  (apply (car matcher)
+						 (list (cadr matcher)
+						       :face 'all-the-icons-dired-dir-face
+						       :v-adjust all-the-icons-dired-v-adjust)))))))
+			  (insert icon))
+		      (insert (all-the-icons-icon-for-file file :v-adjust all-the-icons-dired-v-adjust))))
+		  (insert "\t"))))   ; Align and keep one space for refeshing after operations
+	    (forward-line 1))))))
+  (advice-add #'all-the-icons-dired--display :override #'my:all-the-icons-dired--display))
 
 ;; Local Variables:
 ;; no-byte-compile: t

8e4a02955cad88f7e53282f638c346b34525e6b3
minorugh <minorugh@gmail.com>
2020-04-16 20:55:19 +0900

    Fix comment

--- inits/90_translate.el
+++ inits/90_translate.el
@@ -40,19 +40,18 @@
   ;; Fix error of "args out of range"
   (leaf *bugfix-tips
     :url "https://qiita.com/akicho8/items/cae976cb3286f51e4632"
-    :doc "Replace function in google-translate-core.el"
+    :doc "Apply the following patch."
     ;; ---------------------------------------------------------------
-    ;; (defun google-translate-json-suggestion (json)
-    ;;   "Retrieve from JSON (which returns by the
-    ;; `google-translate-request' function) suggestion. This function
+    ;; --- a/google-translate-core.el
+    ;; +++ b/google-translate-core.el
+    ;; @@ -252,7 +252,7 @@ speech."
     ;; does matter when translating misspelled word. So instead of
     ;; translation it is possible to get suggestion."
-    ;;   (let ((info (aref json 7)))
-    ;; -  (when info
-    ;; -  (aref info 1))))
-    ;; +    (when (and info (> (length info) 0))
-    ;; +        (aref info 1)
-    ;; +      nil)))
+    ;; (let ((info (aref json 7)))
+    ;; - (when info
+    ;; + (when (and info (> (length info) 0))
+    ;; (aref info 1))))
+    ;; (defun google-translate-version ()
     ;; -----------------------------------------------------------------
     ))
 

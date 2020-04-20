2de90d0c3a677a16b7675366c1f9bea6faa42221
minorugh <minorugh@gmail.com>
2020-04-20 21:03:52 +0900

    Fix markdown-preview setting

--- inits/50_markdown.el
+++ inits/50_markdown.el
@@ -131,45 +131,6 @@ document.addEventListener('DOMContentLoaded', () => {
 		 ".docx"))))))
 
 
-;; localstackと9000がぶつかるので変えた
-;; (setq markdown-preview-http-port 19000)
-;; (setq markdown-command "runpandoc")
-;; (setq markdown-use-pandoc-style-yaml-metadata t)
-
-;; ;; ショッキングピンクとか使ってて見た目がやばいので変える
-;; (setq markdown-preview-stylesheets nil)
-
-
-;; ;; markdown-preview like github
-(setq markdown-command "pandoc"
-      markdown-command-needs-filename t
-      markdown-content-type "application/xhtml+xml"
-      markdown-css-paths '("https://cdn.jsdelivr.net/npm/github-markdown-css/github-markdown.min.css"
-			   "http://cdn.jsdelivr.net/gh/highlightjs/cdn-release/build/styles/github.min.css")
-      markdown-xhtml-header-content "
-<meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
-<style>
-body {
-  box-sizing: border-box;
-  max-width: 740px;
-  width: 100%;
-  margin: 40px auto;
-  padding: 0 10px;
-}
-</style>
-<script src='http://cdn.jsdelivr.net/gh/highlightjs/cdn-release/build/highlight.min.js'></script>
-<script>
-document.addEventListener('DOMContentLoaded', () => {
-  document.body.classList.add('markdown-body');
-  document.querySelectorAll('pre[lang] > code').forEach((code) => {
-    code.classList.add(code.parentElement.lang);
-    hljs.highlightBlock(code);
-  });
-});
-</script>
-")
-
-
 ;; Local Variables:
 ;; no-byte-compile: t
 ;; End:

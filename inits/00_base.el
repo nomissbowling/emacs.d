34fa56c928e0dbf0a383e4609b2be6dfd1dcd2d9
minorugh <minorugh@gmail.com>
2020-04-23 09:48:18 +0900

    Refactor

--- inits/00_base.el
+++ inits/00_base.el
@@ -3,16 +3,16 @@
 ;;; Code:
 ;; (setq debug-on-error t)
 
-(leaf server
-  :require t
-  :config
-  (unless (server-running-p)
-    (server-start)))
-
-
 ;; Save the file specified code with basic utf-8 if it exists
 (set-language-environment "Japanese")
 (prefer-coding-system 'utf-8)
+
+
+(leaf exec-path-from-shell :require t
+  :when  (memq window-system '(mac ns x))
+  :hook (after-init-hook . exec-path-from-shell-initialize)
+  :config
+  (setq exec-path-from-shell-check-startup-files nil))
 
 
 ;; font for main-machine
@@ -26,106 +26,115 @@
   (add-to-list 'default-frame-alist '(font . "Cica-14.5")))
 
 
-(leaf exec-path-from-shell
+(leaf server
   :require t
-  :when  (member window-system '(mac ns x))
-  :hook (after-init-hook . exec-path-from-shell-initialize)
   :config
-  (setq exec-path-from-shell-check-startup-files nil))
+  (unless (server-running-p)
+    (server-start)))
 
 
-(leaf Cus-start
+(leaf custom-start
   :custom
   (;; Display file name in title bar: buffername-emacs-version
    (frame-title-format . "%b")
+
    ;; Point keeps its screen position when scroll
    (scroll-preserve-screen-position . :always)
+
    ;; Turn Off warning sound screen flash
    (visible-bell . nil)
+
    ;; All warning sounds and flash are invalid (note that the warning sound does not sound completely)
    (ring-bell-function . 'ignore)
+
    ;; Do not change the position of the cursor when scrolling pages
    (scroll-preserve-screen-position . t)
+
    ;; Suppress warnings for 'ad-handle-definition:'
    (ad-redefinition-action . 'accept)
+
    ;; Faster rendering by not corresponding to right-to-left language
    (bidi-display-reordering . nil)
+
    ;; between the lines
    (line-spacing . 0.1)
+
    ;; Do not distinguish uppercase and lowercase letters on completion
    (completion-ignore-case . t)
    (read-file-name-completion-ignore-case . t)
+
    ;; Copy with mouse drag
    (mouse-drag-copy-region . t)
+
    ;; Do not make a backup filie like *.~
    (make-backup-files . nil)
+
    ;; Do not use auto save
    (auto-save-default . nil)
+
    ;; Do not create lock file
    (create-lockfiles . nil)
+
    ;; Do not record the same content in the history
    (history-delete-duplicates . t))
 
   :hook
-  (;; Automatic reloading of changed files
+  (;; Save hist
+   (after-init-hook . savehist-mode)
+
+   ;;Save place
+   (after-init-hook . save-place-mode)
+
+   ;; Automatic reloading of changed files
    (after-init-hook . global-auto-revert-mode)
+
    ;; Do not blink the cursor
    (after-init-hook . blink-cursor-mode)
+
    ;; font-lock
    (after-init-hook . global-font-lock-mode)
+
    ;; word wrapping is used
    (after-init-hook . global-visual-line-mode)
+
    ;; Turn off 'Suspicious line XXX of Makefile.' makefile warning
    (makefile-mode-hook
     (lambda ()
       (fset 'makefile-warn-suspicious-lines 'ignore))))
 
   :init
-  ;; Set makefle mode
-  (add-to-list 'auto-mode-alist '("\\.mak\\'" . makefile-mode))
   ;; Exit Emacs with M-x exitle
   (defalias 'exit 'save-buffers-kill-emacs)
+
   ;; Input yes or no to y or n (even SPC OK instead of y)
   (defalias 'yes-or-no-p 'y-or-n-p)
+
   ;; Set transparency (active inactive)
   (add-to-list 'default-frame-alist '(alpha . (1.0 0.8)))
-  )
 
+  ;; Set makefle mode
+  (add-to-list 'auto-mode-alist '("\\.mak\\'" . makefile-mode))
 
-(leaf leaf
-  :config
-  (leaf save-place
-    :hook (after-init-hook . save-place-mode))
-
-  (leaf savehist
-    :doc "Save history of minibuffer."
-    :hook (after-init-hook . savehist-mode)
-    :custom (history-length . 1000))
-
-  (leaf uniquify
-    :doc "Make it easy to see when it is the same name file."
-    :custom ((uniquify-buffer-name-style . 'post-forward-angle-brackets)
-	     (uniquify-min-dir-content . 1)))
-
-  (leaf generic-x
-    :doc "contains many mode setting")
-
+  ;; Set web mode
   (leaf web-mode :ensure t
     :mode (("\\.html?\\'" . web-mode)))
 
-  (leaf select
-    :doc "use the X11 clipboard."
-    :bind (("M-w" . clipboard-kill-ring-save)
-	   ("C-w" . my:clipboard-kill-region))
-    :custom (select-enable-clipboard . t)
-    :preface
-    (defun my:clipboard-kill-region ()
-      "If the region is active, `clipboard-kill-region'. If the region is inactive, `backward-kill-word'."
-      (interactive)
-      (if (use-region-p)
-	  (clipboard-kill-region (region-beginning) (region-end))
-	(backward-kill-word 1))))
+  ;; Set buffer that can not be killed
+  (with-current-buffer "*scratch*"
+    (emacs-lock-mode 'kill))
+  (with-current-buffer "*Messages*"
+    (emacs-lock-mode 'kill))
 
+  :preface
+  ;; Make it easy to see when it is the same name file
+  (leaf uniquify
+    :custom ((uniquify-buffer-name-style . 'post-forward-angle-brackets)
+	     (uniquify-min-dir-content . 1)))
+
+  ;; contains many mode setting
+  (leaf generic-x)
+
+  ;; Recentf
   (leaf recentf
     :hook (after-init-hook . recentf-mode)
     :custom((recentf-save-file . "~/.emacs.d/recentf")
@@ -136,42 +145,47 @@
 		 "\\.\\(?:gz\\|gif\\|svg\\|png\\|jpe?g\\)$" "\\.howm" "^/tmp/" "^/ssh:" "^/scp"
 		 (lambda (file) (file-in-directory-p file package-user-dir)))))
     :config
-    (push (expand-file-name recentf-save-file) recentf-exclude))
-  )
+    (push (expand-file-name recentf-save-file) recentf-exclude)))
 
 
+;; Key modified
 (leaf bind-key
   :ensure t
-  :bind (("<insert>" . clipboard-yank)
-	 ("C-." . xref-find-definitions)
+  :bind (("C-." . xref-find-definitions)
 	 ("M-/" . kill-buffer)
-	 ("C-M-/" . kill-other-buffer))
+	 ("C-M-/" . kill-other-buffer)
+	 ("M-w" . clipboard-kill-ring-save)
+	 ("C-w" . my:clipboard-kill-region))
   :bind* (("<muhenkan>" . minibuffer-keyboard-quit)
 	  ("C-x C-c" . iconify-frame))
+  :custom (select-enable-clipboard . t)
+
   :config
   (defun kill-other-buffers ()
     "Kill all other buffers."
     (interactive)
     (mapc 'kill-buffer (delq (current-buffer) (buffer-list)))
-    (message "Killed other buffers!")))
+    (message "Killed other buffers!"))
+
+  (defun my:clipboard-kill-region ()
+    "If the region is active, `clipboard-kill-region'.
+If the region is inactive, `backward-kill-word'."
+    (interactive)
+    (if (use-region-p)
+	(clipboard-kill-region (region-beginning) (region-end))
+      (backward-kill-word 1))))
 
 
-(leaf Emacs-manual
-  :doc "M-x info-emacs-manual (C-h r or F1+r)"
-  :config
-  (add-to-list 'Info-directory-list "~/Dropbox/emacs.d/info/")
-  (advice-add 'Info-find-node :around 'Info-find-node--info-ja)
-  (defun Info-find-node--info-ja (orig-fn filename &rest args)
-    "Info as ORIG-FN FILENAME ARGS."
-    (apply orig-fn
-	   (pcase filename
-	     ("emacs" "emacs-ja.info")
-	     (_ filename))
-	   args)))
-
-
-;; Load my template
-(load "~/Dropbox/emacs.d/elisp/my-template.el")
+;; M-x info-emacs-manual (C-h r or F1+r)
+(add-to-list 'Info-directory-list "~/Dropbox/emacs.d/info/")
+(defun Info-find-node--info-ja (orig-fn filename &rest args)
+  "Info as ORIG-FN FILENAME ARGS."
+  (apply orig-fn
+	 (pcase filename
+	   ("emacs" "emacs-ja.info")
+	   (_ filename))
+	 args))
+(advice-add 'Info-find-node :around 'Info-find-node--info-ja)
 
 
 ;; Local Variables:

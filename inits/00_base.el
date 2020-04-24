e689fb84c1011b8d7591519b9067dc8bbdd41c06
minorugh <minorugh@gmail.com>
2020-04-24 18:45:50 +0900

    Add load file

--- inits/00_base.el
+++ inits/00_base.el
@@ -158,6 +158,9 @@ If the region is inactive, `backward-kill-word'."
 (advice-add 'Info-find-node :around 'Info-find-node--info-ja)
 
 
+;; Load user defined elisp
+(load "~/Dropbox/emacs.d/elisp/user-defined.el")
+
 ;; Local Variables:
 ;; no-byte-compile: t
 ;; End:

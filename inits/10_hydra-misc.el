859185b1613fa0cd0d139457e6aea3576d48b79d
minorugh <minorugh@gmail.com>
2020-04-25 20:28:18 +0900

    Fix header

--- inits/10_hydra-misc.el
+++ inits/10_hydra-misc.el
@@ -1,4 +1,4 @@
-;;; 10_hydra-misc.el --- 10_hydra-misc.el  -*- lexical-binding: t; -*-
+;;; 10_hydra-misc.el --- 10_hydra-misc.el  -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
 ;; (setq debug-on-error t)

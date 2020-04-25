0891749a1a4190ba083747425604599878f0aaec
minorugh <minorugh@gmail.com>
2020-04-25 20:19:18 +0900

    Fix comment

--- inits/02_git.el
+++ inits/02_git.el
@@ -1,6 +1,6 @@
-;;; 02_git.el --- 02_git.el  -*- lexical-binding: t; -*-
-;;; Commentary:
-;;; Code:
+;;; 02_git.el --- 02_git.el  -*- lexical-binding: t -*-
+;; Git porcelain inside Emacs.
+
 ;; (setq debug-on-erro
 
 (leaf magit

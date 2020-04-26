cbebd717dda713663f8618d1f8bb81efb76466be
minorugh <minorugh@gmail.com>
2020-04-26 10:19:32 +0900

    Fix

--- init.el
+++ init.el
@@ -12,15 +12,6 @@
 (setq inhibit-startup-message t)
 (setq gc-cons-threshold (* 128 1024 1024))
 
-;; this enables this running methodf
-;; emacs -q -l ~/.debug.emacs.d/{{pkg}}/init.el
-(eval-and-compile
-  (when (or load-file-name byte-compile-current-file)
-    (setq load-prefer-newer t)
-    (setq user-emacs-directory
-          (expand-file-name
-           (file-name-directory (or load-file-name byte-compile-current-file))))))
-
 (eval-and-compile
   (customize-set-variable
    'package-archives '(("org"   . "https://orgmode.org/elpa/")
@@ -34,6 +25,7 @@
     :ensure t
     :init
     (leaf leaf-convert :ensure t)
+    (leaf init-loader :ensure t)
     (leaf bind-key :ensure t)
     (leaf hydra :ensure t)
     (leaf el-get :ensure t)
@@ -41,17 +33,11 @@
     (setq el-get-dir "~/.emacs.d/elisp")
     (leaf-keywords-init)))
 
-(leaf macrostep
-  :ensure t
-  :bind (("C-c e" . macrostep-expand)))
-
-(leaf init-loader
-  :ensure t
-  :config
-  (custom-set-variables
-   '(init-loader-show-log-after-init 'error-only))
-  (init-loader-load "~/Dropbox/emacs.d/inits")
-  (setq custom-file (locate-user-emacs-file "custom.el")))
+;; init-loader
+(custom-set-variables
+ '(init-loader-show-log-after-init 'error-only))
+(init-loader-load "~/Dropbox/emacs.d/inits")
+(setq custom-file (locate-user-emacs-file "custom.el"))
 
 
 (provide 'init)

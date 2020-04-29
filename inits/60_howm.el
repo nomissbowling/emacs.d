c25fa53da39a01669d226956496331562280fb45
minorugh <minorugh@gmail.com>
2020-04-29 16:54:45 +0900

    Delete hydra-howm

--- inits/60_howm.el
+++ inits/60_howm.el
@@ -30,89 +30,12 @@
 	  ("日記:" . (0 'diary prepend))
 	  ("note:" . (0 'diff-changed prepend)))))
 
-;;  :hydra
-;;  (hydra-howm
-;;   (:hint nil :exit t)
-;;   "
-;; 📝 howm:  memo_,_  _i_dea  _h_ack  _n_ote  _d_iary  _p_age  _b_log  _m_ail  list_@_  _j_unk"
-;;   ("," my:howm-memo)
-;;   ("i" my:howm-idea)
-;;   ("h" my:howm-hack)
-;;   ("n" my:howm-note)
-;;   ("d" my:howm-dia)
-;;   ("p" my:howm-page)
-;;   ("b" my:howm-blog)
-;;   ("m" my:howm-mail)
-;;   ("@" howm-list-all)
-;;   ("j" open-junk-file)
-;;   ("q" nil)))
-
 
 (leaf open-junk-file
   :ensure t
   :config
   (setq open-junk-file-format "~/Dropbox/howm/junk/%Y/%Y%m%d.")
   (setq open-junk-file-find-file-function 'find-file))
-
-
-;; (leaf *user-howm-function
-;;   :config
-;;   (defun my:howm-memo ()
-;;     "My howm remember for memo."
-;;     (interactive)
-;;     (setq howm-template (concat howm-view-title-header "%title memo: %cursor\n"))
-;;     (howm-remember))
-;;   (defun my:howm-idea ()
-;;     "My howm remember for idea."
-;;     (interactive)
-;;     (setq howm-template (concat howm-view-title-header "%title idea: %cursor\n"))
-;;     (howm-create))
-;;   (defun my:howm-hack ()
-;;     "My howm create for hack."
-;;     (interactive)
-;;     (setq howm-template (concat howm-view-title-header "%title hack: %cursor\nurl:\n"))
-;;     (howm-create))
-;;   (defun my:howm-mail ()
-;;     "My howm remember for mail draft."
-;;     (interactive)
-;;     (setq howm-template (concat howm-view-title-header "%title mail: %cursor\n"))
-;;     (howm-create))
-;;   (defun my:howm-blog ()
-;;     "My howm create for blog draft."
-;;     (interactive)
-;;     (setq howm-template (concat howm-view-title-header "%title blog: %cursor\n%date\n"))
-;;     (howm-create))
-;;   (defun my:howm-dia ()
-;;     "My howm create for diary draft."
-;;     (interactive)
-;;     (setq howm-template (concat howm-view-title-header "%title 日記: %cursor\n%date\n"))
-;;     (howm-create))
-;;   (defun my:howm-page ()
-;;     "My howm create for page draft."
-;;     (interactive)
-;;     (setq howm-template (concat howm-view-title-header "%title page: %cursor\n%date\n"))
-;;     (howm-create))
-;;   (defun my:howm-note ()
-;;     "My howm create for note draft."
-;;     (interactive)
-;;     (setq howm-template (concat howm-view-title-header "%title note: %cursor\n%date\n"))
-;;     (howm-create))
-;;   (defun my:haiku-note ()
-;;     "Open haiku note file."
-;;     (interactive)
-;;     (find-file (format-time-string "~/Dropbox/howm/haiku/haikunote.%Y.md"))
-;;     (goto-char (point-min)))
-;;   (defun my:haiku-note-post ()
-;;     "Insert template."
-;;     (interactive)
-;;     (find-file (format-time-string "~/Dropbox/howm/haiku/haikunote.%Y.md"))
-;;     (goto-char (point-min))
-;;     (forward-line 2)
-;;     (insert
-;;      (format-time-string "> %Y年%m月%d日 (%a)\n")
-;;      (format-time-string "PLACE:\n\n"))
-;;     (forward-line -2)
-;;     (forward-char 6)))
 
 
 ;; Local Variables:

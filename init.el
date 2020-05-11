a5a5f5d1e89d1b56f0d71fbdbadd0745fc11d9f1
minorugh <minorugh@gmail.com>
2020-05-11 23:15:17 +0900

    Refactor

--- init.el
+++ init.el
@@ -14,42 +14,42 @@
            (file-name-directory (or load-file-name byte-compile-current-file))))))
 
 (eval-and-compile
-  ;; Quiet start
-  (menu-bar-mode 0)
-  (tool-bar-mode 0)
-  (scroll-bar-mode 0)
-  (set-frame-parameter nil 'fullscreen 'maximized)
-  (setq inhibit-splash-screen t)
-  (setq inhibit-startup-message t)
+;; Quiet start
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+(set-frame-parameter nil 'fullscreen 'maximized)
+(setq inhibit-splash-screen t)
+(setq inhibit-startup-message t)
 
-  ;; Increase threshold to fire garbage collection
-  (setq gc-cons-threshold (* 128 1024 1024))
+;; Increase threshold to fire garbage collection
+(setq gc-cons-threshold (* 128 1024 1024))
 
-  (customize-set-variable
-   'package-archives '(("org"   . "https://orgmode.org/elpa/")
-                       ("melpa" . "https://melpa.org/packages/")))
-  (package-initialize)
-  (unless (package-installed-p 'leaf)
-    (package-refresh-contents)
-    (package-install 'leaf))
+(customize-set-variable
+ 'package-archives '(("org"   . "https://orgmode.org/elpa/")
+                     ("melpa" . "https://melpa.org/packages/")))
+(package-initialize)
+(unless (package-installed-p 'leaf)
+  (package-refresh-contents)
+  (package-install 'leaf))
 
-  (leaf leaf-keywords
-    :ensure t
-    :init
-    (leaf bind-key :ensure t)
-    (leaf hydra :ensure t)
-    (leaf el-get :ensure t)
-    :config
-    (setq el-get-dir "~/.emacs.d/elisp")
-    (leaf-keywords-init))
+(leaf leaf-keywords
+  :ensure t
+  :init
+  (leaf bind-key :ensure t)
+  (leaf hydra :ensure t)
+  (leaf el-get :ensure t)
+  :config
+  (setq el-get-dir "~/.emacs.d/elisp")
+  (leaf-keywords-init))
 
-  (leaf init-loader
-    :ensure t
-    :config
-    (custom-set-variables
-     '(init-loader-show-log-after-init 'error-only))
-    (init-loader-load "~/Dropbox/emacs.d/inits")
-    (setq custom-file (locate-user-emacs-file "custom.el"))))
+(leaf init-loader
+  :ensure t
+  :config
+  (custom-set-variables
+   '(init-loader-show-log-after-init 'error-only))
+  (init-loader-load "~/Dropbox/emacs.d/inits")
+  (setq custom-file (locate-user-emacs-file "custom.el"))))
 
 
 (provide 'init)

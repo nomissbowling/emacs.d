7454a186c1984c17c683c382ae108eb4c325b54b
minorugh <minorugh@gmail.com>
2020-05-13 18:02:40 +0900

    Fix darkroom-mode

--- inits/07_ui.el
+++ inits/07_ui.el
@@ -60,6 +60,7 @@
     (interactive)
     (display-line-numbers-mode 0)
     (flymake-mode 0)
+    (fringe-mode -1)
     (darkroom-mode 1))
 
   (defun my:darkroom-mode-out ()
@@ -67,6 +68,7 @@
     (interactive)
     (darkroom-mode 0)
     (flymake-mode 1)
+    (fringe-mode (cons 0 nil))
     (display-line-numbers-mode 1)))
 
 (leaf all-the-icons

7ea88666b62073a211775c71428cd4cfff93517c
minorugh <minorugh@gmail.com>
2020-05-19 10:31:23 +0900

    Fix package-archives

--- init.el
+++ init.el
@@ -19,7 +19,9 @@
 
   (customize-set-variable
    'package-archives '(("org"   . "https://orgmode.org/elpa/")
-                       ("melpa" . "https://melpa.org/packages/")))
+                       ("melpa" . "https://melpa.org/packages/")
+                       ("gnu"   . "https://elpa.gnu.org/packages/")))
+
   (package-initialize)
   (unless (package-installed-p 'leaf)
     (package-refresh-contents)

7a7b8dd8e27b4f04fa7566aeccf01eb24f075e63
minorugh <minorugh@gmail.com>
2020-06-12 06:55:33 +0900

    Fix startup optimizations

--- init.el
+++ init.el
@@ -16,12 +16,12 @@
   ;; Startup optimizations
   (defvar default-file-name-handler-alist file-name-handler-alist)
   (setq file-name-handler-alist nil)
-  (setq gc-cons-threshold  (* 1024 1024 100))
+  (setq gc-cons-threshold 80000000)
   (add-hook 'emacs-startup-hook
 	    (lambda ()
 	      "Restore defalut values after init."
 	      (setq file-name-handler-alist default-file-name-handler-alist)
-	      (setq gc-cons-threshold  (* 1024 1024 1024))
+	      (setq gc-cons-threshold 800000)
 	      (if (boundp 'after-focus-change-function)
 		  (add-function :after after-focus-change-function
 				(lambda ()

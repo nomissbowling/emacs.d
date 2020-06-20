5ec29fc498a8fae201e36426820d0349a0f2184f
minorugh <minorugh@gmail.com>
2020-06-20 09:58:17 +0900

    Add mozc-tool functions

--- inits/30_mozc.el
+++ inits/30_mozc.el
@@ -6,10 +6,16 @@
 (leaf mozc
   :ensure t
   :bind* ("<hiragana-katakana>" . toggle-input-method)
+  :bind ("<f8>" . my:select-mozc-tool)
   :config
   (setq default-input-method "japanese-mozc"
 	mozc-helper-program-name "mozc_emacs_helper"
 	mozc-leim-title "♡かな")
+  (defadvice toggle-input-method (around toggle-input-method-around activate)
+    "Input method function in key-chord.el not to be nil."
+    (let ((input-method-function-save input-method-function))
+      ad-do-it
+      (setq input-method-function input-method-function-save)))
   :init
   (leaf mozc-temp
     :ensure t
@@ -33,11 +39,30 @@
 
 (leaf *user-mozc-function
   :init
-  (defadvice toggle-input-method (around toggle-input-method-around activate)
-    "Input method function in key-chord.el not to be nil."
-    (let ((input-method-function-save input-method-function))
-      ad-do-it
-      (setq input-method-function input-method-function-save)))
+  (defun my:select-mozc-tool ()
+    "Narrow the only espy command in M-x."
+    (interactive)
+    (counsel-M-x "^my:mozc "))
+
+  (defun my:mozc-config-dialog ()
+    "Run the mozc-tool in the background."
+    (interactive)
+    (compile "/usr/lib/mozc/mozc_tool --mode=config_dialog"))
+
+  (defun my:mozc-dictionary-tool ()
+    "Run the mozc-tool in the background."
+    (interactive)
+    (compile "/usr/lib/mozc/mozc_tool --mode=dictionary_tool"))
+
+  (defun my:mozc-word-regist ()
+    "Run the mozc-tool in the background."
+    (interactive)
+    (compile "/usr/lib/mozc/mozc_tool --mode=word_register_dialog"))
+
+  (defun my:mozc-hand-writing ()
+    "Run the mozc-tool in the background."
+    (interactive)
+    (compile "/usr/lib/mozc/mozc_tool --mode=hand_writing"))
 
   (defun mozc-insert-str (str)
     "If punctuation marks, immediately confirm."

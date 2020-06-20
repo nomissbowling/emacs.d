26954f227d3b9c0e545c0ee4bac9d5bcff76fee6
minorugh <minorugh@gmail.com>
2020-06-20 12:34:29 +0900

    Refactor

--- inits/30_mozc.el
+++ inits/30_mozc.el
@@ -11,11 +11,23 @@
   (setq default-input-method "japanese-mozc"
 	mozc-helper-program-name "mozc_emacs_helper"
 	mozc-leim-title "♡かな")
+  :preface
   (defadvice toggle-input-method (around toggle-input-method-around activate)
     "Input method function in key-chord.el not to be nil."
     (let ((input-method-function-save input-method-function))
       ad-do-it
       (setq input-method-function input-method-function-save)))
+  (defun mozc-insert-str (str)
+    "If punctuation marks, immediately confirm."
+    (mozc-handle-event 'enter)
+    (toggle-input-method)
+    (insert str)
+    (toggle-input-method))
+  (add-hook 'mozc-mode-hook
+	    (lambda ()
+	      (define-key mozc-mode-map "?" '(lambda () (interactive) (mozc-insert-str "？")))
+	      (define-key mozc-mode-map "," '(lambda () (interactive) (mozc-insert-str "、")))
+	      (define-key mozc-mode-map "." '(lambda () (interactive) (mozc-insert-str "。")))))
   :init
   (leaf mozc-temp
     :ensure t
@@ -37,7 +49,7 @@
     (leaf posframe :ensure t)))
 
 
-(leaf *user-mozc-function
+(leaf *user-mozc-tool
   :init
   (defun my:select-mozc-tool ()
     "Narrow the only espy command in M-x."
@@ -47,34 +59,22 @@
   (defun my:mozc-config-dialog ()
     "Run the mozc-tool in the background."
     (interactive)
-    (compile "/usr/lib/mozc/mozc_tool --mode=config_dialog"))
+    (async-shell-command "/usr/lib/mozc/mozc_tool --mode=config_dialog"))
 
   (defun my:mozc-dictionary-tool ()
     "Run the mozc-tool in the background."
     (interactive)
-    (compile "/usr/lib/mozc/mozc_tool --mode=dictionary_tool"))
+    (async-shell-command "/usr/lib/mozc/mozc_tool --mode=dictionary_tool"))
 
   (defun my:mozc-word-regist ()
     "Run the mozc-tool in the background."
     (interactive)
-    (compile "/usr/lib/mozc/mozc_tool --mode=word_register_dialog"))
+    (async-shell-command "/usr/lib/mozc/mozc_tool --mode=word_register_dialog"))
 
   (defun my:mozc-hand-writing ()
     "Run the mozc-tool in the background."
     (interactive)
-    (compile "/usr/lib/mozc/mozc_tool --mode=hand_writing"))
-
-  (defun mozc-insert-str (str)
-    "If punctuation marks, immediately confirm."
-    (mozc-handle-event 'enter)
-    (toggle-input-method)
-    (insert str)
-    (toggle-input-method))
-  (add-hook 'mozc-mode-hook
-	    (lambda ()
-	      (define-key mozc-mode-map "?" '(lambda () (interactive) (mozc-insert-str "？")))
-	      (define-key mozc-mode-map "," '(lambda () (interactive) (mozc-insert-str "、")))
-	      (define-key mozc-mode-map "." '(lambda () (interactive) (mozc-insert-str "。"))))))
+    (async-shell-command "/usr/lib/mozc/mozc_tool --mode=hand_writing")))
 
 
 ;; Local Variables:

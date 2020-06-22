948af3140f7571b9751c1fa1fecae38ffa95bc55
minorugh <minorugh@gmail.com>
2020-06-22 16:54:58 +0900

    Refactor

--- inits/70_yatex.el
+++ inits/70_yatex.el
@@ -6,38 +6,40 @@
 (leaf yatex
   :ensure t
   :mode ("\\.tex\\'" . yatex-mode)
-  :custom
-  ((tex-command . "platex")
-   (dviprint-command-format . "dvpd.sh %s")
-   (YaTeX-kanji-code . nil)
-   (YaTeX-latex-message-code . 'utf-8)
-   (Section-name . "documentclass")
-   (makeindex-command . "mendex")
-   (YaTeX-use-AMS-LaTeX . t)
-   (YaTeX-use-LaTeX2e . t)
-   (YaTeX-use-font-lock . t)
-   (YaTeX-default-pop-window-height . 20))
+  :config
+  (setq tex-command "platex"
+	dviprint-command-format "dvpd.sh %s"
+	YaTeX-kanji-code nil
+	YaTeX-latex-message-code 'utf-8
+	YaTeX-default-pop-window-height 25)
   :init
-  (add-hook 'yatex-mode-hook
-	    '(lambda ()
-	       (leaf yatexprc
-		 :bind (("M-c" . YaTeX-typeset-buffer)	;; Type set buffer
-			("M-l" . YaTeX-lpr))))))	;; Open pdf veiwer
+  (add-hook
+   'yatex-mode-hook
+   '(lambda ()
+      (leaf yatexprc
+  	:bind (("M-c" . YaTeX-typeset-buffer)	;; Type set buffer
+  	       ("M-l" . YaTeX-lpr))))))		;; Open pdf veiwer
 
 
 ;; Dviprint-command-format
 ;; -----------------------------------------------------------------------
 ;; dvpd.sh for Linux
 ;; Create dvpd.sh and execute 'chmod +x', and place it in `/usr/local/bin'
+;;
+;; for Linux
 ;; | #!/bin/bash
 ;; | name=$1
 ;; | dvipdfmx $1 && evince ${name%.*}.pdf
-
-;; dvpd.sh for WSL
-;; Create dvpd.sh and execute 'chmod +x', and place it in `/usr/local/bin'
+;; |# Delete unnecessary files
+;; |rm *.au* *.dv* *.lo*
+;;
+;; for WSL
 ;; | #!/bin/bash
 ;; | name=$1
 ;; | dvipdfmx $1 && wslstart ${name%.*}.pdf
+;; |# Delete unnecessary files
+;; |rm *.au* *.dv* *.lo*
+;;
 ;; ------------------------------------------------------------------------
 
 

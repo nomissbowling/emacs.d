573999d09dcc9479002b2cab8c73ea5e36f3e653
minorugh <minorugh@gmail.com>
2020-06-22 09:42:16 +0900

    Fix close-compilation-window

--- inits/10_hydra-make.el
+++ inits/10_hydra-make.el
@@ -21,18 +21,19 @@
 
 (leaf *user-make-function
   :init
-  (defun bury-compile-buffer-if-successful (buffer string)
-    "Delete a compilation window if succeeded without warnings."
-    (if (and
-	 (string-match "compilation" (buffer-name buffer))
-	 (string-match "finished" string)
-	 (not
-	  (with-current-buffer buffer
-	    (search-forward "warning" nil t))))
-	(run-with-timer 1 nil
-			(lambda ()
-			  (delete-other-windows)))))
-  (add-hook 'compilation-finish-functions 'bury-compile-buffer-if-successful)
+  ;; https://gist.github.com/EricCrosson/fa41233f327403ea2a5a
+  (defun close-compile-window-if-successful (buffer string)
+    "Close a compilation window if succeeded without warnings."
+    (when (and
+	   (string-match "compilation" (buffer-name buffer))
+	   (string-match "finished" string)
+	   (not
+	    (with-current-buffer buffer
+	      (search-forward "warning" nil t))))
+      (run-with-timer 1 nil
+		      (lambda ()
+			(delete-other-windows)))))
+  (add-hook 'compilation-finish-functions 'close-compile-window-if-successful)
   :config
   (defun my:make-k ()
     "Make command default."

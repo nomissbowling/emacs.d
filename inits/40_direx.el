8782b471f0c26fc311ee9d385ff4206e19736e69
minorugh <minorugh@gmail.com>
2020-06-28 15:29:06 +0900

    New file

---
+++ inits/40_direx.el
@@ -0,0 +1,31 @@
+;;; 40_direx.el --- 40_direx.el  -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+;; (setq debug-on-error t)
+
+(leaf direx
+  :ensure t
+  :after popwin
+  :bind (("<f10>" . direx:jump-to-project-directory)
+	 (:direx:direx-mode-map
+	  ("<f10>" . quit-window)))
+  :config
+  (setq direx:leaf-icon "  " direx:open-icon "📂" direx:closed-icon "📁")
+  (push '(direx:direx-mode :position left :width 25 :dedicated t) popwin:special-display-config)
+  :init
+  ;; https://blog.shibayu36.org/entry/2013/02/12/191459
+  (defun direx:jump-to-project-directory ()
+    "If in project, launch direx-project otherwise start direx."
+    (interactive)
+    (let ((result (ignore-errors
+		    (direx-project:jump-to-project-root-other-window)
+		    t)))
+      (unless result
+	(direx:jump-to-directory-other-window)))))
+
+
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
+
+;;; 40_direx.el ends here

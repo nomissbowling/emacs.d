0358f8a816c72c547261c96c4d0dede3c94dbf49
minorugh <minorugh@gmail.com>
2020-07-06 15:41:13 +0900

    Move set buffer that can not killed

--- inits/09_buffer.el
+++ inits/09_buffer.el
@@ -15,12 +15,6 @@
   (setq auto-save-buffers-enhanced-exclude-regexps '("^/ssh:" "^/scp:" "/sudo:"))
   (auto-save-buffers-enhanced t))
 
-;; Set buffer that can not be killed
-(with-current-buffer "*scratch*"
-  (emacs-lock-mode 'kill))
-(with-current-buffer "*Messages*"
-  (emacs-lock-mode 'kill))
-
 ;; automatically deleted in the background buffers
 (leaf tempbuf
   :el-get emacswiki:tempbuf

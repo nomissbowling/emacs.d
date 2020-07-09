9a234cba737c2e32910f72f7bb53ce5afb0ac5f3
minorugh <minorugh@gmail.com>
2020-07-09 21:19:58 +0900

    Refactor

--- init.el
+++ init.el
@@ -16,8 +16,8 @@
   	      (setq gc-cons-threshold 800000)))
 
   (customize-set-variable
-   'package-archives '(("melpa" . "https://melpa.org/packages/")
-		       ("org"   . "https://orgmode.org/elpa/")
+   'package-archives '(("org"   . "https://orgmode.org/elpa/")
+		       ("melpa" . "https://melpa.org/packages/")
 		       ("gnu"   . "https://elpa.gnu.org/packages/")))
 
   (package-initialize)

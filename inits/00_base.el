9e8bc7c20150408c3e527cc7464ca34aa7a542df
minorugh <minorugh@gmail.com>
2020-07-18 06:43:01 +0900

    Refactor

--- inits/00_base.el
+++ inits/00_base.el
@@ -15,151 +15,180 @@
   (setq inhibit-splash-screen t)
   (setq inhibit-startup-message t)
 
+
   ;; Save the file specified code with basic utf-8 if it exists
   (set-language-environment "Japanese")
   (prefer-coding-system 'utf-8)
+
 
   ;; Recentf
   (leaf recentf
     :hook (after-init-hook . recentf-mode)
     :config
     (setq recentf-save-file "~/.emacs.d/recentf"
-  	  recentf-max-saved-items 200
-  	  recentf-auto-cleanup 'never
-  	  recentf-exclud '("recentf" "COMMIT_EDITMSG\\" "bookmarks" "emacs\\．d" "\\.gitignore"
-  			   "\\.\\(?:gz\\|gif\\|svg\\|png\\|jpe?g\\)$" "\\.howm" "^/tmp/" "^/scp:"
-  			   (lambda (file) (file-in-directory-p file package-user-dir))))
+	  recentf-max-saved-items 200
+	  recentf-auto-cleanup 'never
+	  recentf-exclud '("recentf" "COMMIT_EDITMSG\\" "bookmarks" "emacs\\．d" "\\.gitignore"
+			   "\\.\\(?:gz\\|gif\\|svg\\|png\\|jpe?g\\)$" "\\.howm" "^/tmp/" "^/scp:"
+			   (lambda (file) (file-in-directory-p file package-user-dir))))
     (push (expand-file-name recentf-save-file) recentf-exclude))
 
-  ;; Startup hook section
-  (add-hook
-   'emacs-startup-hook
-   (lambda ()
-     ;; font
-     (add-to-list 'default-frame-alist '(font . "Cica-18"))
-     ;; for sub-machine
-     (when (string-match "x250" (shell-command-to-string "uname -n"))
-       (add-to-list 'default-frame-alist '(font . "Cica-15")))
 
-     ;; Emacs use the $PATH set up by the user's shell
-     (leaf exec-path-from-shell :ensure t
-       :config
-       (setq exec-path-from-shell-check-startup-files nil)
-       (exec-path-from-shell-initialize))
+  (leaf *user-emacs-init-time
+    :hook (after-init-hook . my:emacs-init-time)
+    :init
+    (defun my:emacs-init-time ()
+      "Emacs booting time in msec."
+      (message "Emacs booting time: %.0f [msec] = `emacs-init-time'."
+  	       (* 1000
+  		  (float-time (time-subtract
+  			       after-init-time
+  			       before-init-time)))))
 
-     ;; Start the server in Emacs session
-     (leaf server :require t
-       :config
-       (unless (server-running-p)
-	 (server-start)))
+    (with-eval-after-load "time"
+      (defun ad:emacs-init-time ()
+  	"Return a string giving the duration of the Emacs initialization."
+  	(interactive)
+  	(let ((str
+  	       (format "%.3f seconds"
+  		       (float-time
+  			(time-subtract after-init-time before-init-time)))))
+  	  (if (called-interactively-p 'interactive)
+  	      (message "%s" str)
+  	    str)))
+      (advice-add 'emacs-init-time :override #'ad:emacs-init-time)))
 
-     ;; Hide menu-bar
-     (menu-bar-mode 0)
-     ;; Save hist
-     (savehist-mode)
-     ;;Save plae
-     (save-place-mode)
-     ;; Automatic reloading of changed files
-     (global-auto-revert-mode)
-     ;; Do not blink the cursor
-     (blink-cursor-mode)
-     ;; Window configuration undo/redo
-     (winner-mode)
-     ;; font-lock
-     (global-font-lock-mode)
-     ;; word wrapping is used
-     (global-visual-line-mode)
 
-     ;; All warning sounds and flash are invalid (note that the warning sound does not sound completely)
-     (setq ring-bell-function 'ignore)
+  (leaf *Startup-hook-section
+    :init
+    (add-hook
+     'emacs-startup-hook
+     (lambda ()
+       ;; font
+       (add-to-list 'default-frame-alist '(font . "Cica-18"))
+       ;; for sub-machine
+       (when (string-match "x250" (shell-command-to-string "uname -n"))
+	 (add-to-list 'default-frame-alist '(font . "Cica-15")))
 
-     ;; unable right-to-left language reordering
-     (setq-default bidi-display-reordering nil)
+       ;; Emacs use the $PATH set up by the user's shell
+       (leaf exec-path-from-shell :ensure t
+	 :config
+	 (setq exec-path-from-shell-check-startup-files nil)
+	 (exec-path-from-shell-initialize))
 
-     ;; Point keeps its screen position when scroll
-     (setq scroll-preserve-screen-position :always)
+       ;; Start the server in Emacs session
+       (leaf server :require t
+	 :config
+	 (unless (server-running-p)
+	   (server-start)))
 
-     ;; Turn Off warning sound screen flash
-     (setq visible-bell nil)
+       ;; Hide menu-bar
+       (menu-bar-mode 0)
+       ;; Save hist
+       (savehist-mode)
+       ;;Save plae
+       (save-place-mode)
+       ;; Automatic reloading of changed files
+       (global-auto-revert-mode)
+       ;; Do not blink the cursor
+       (blink-cursor-mode)
+       ;; Window configuration undo/redo
+       (winner-mode)
+       ;; font-lock
+       (global-font-lock-mode)
+       ;; word wrapping is used
+       (global-visual-line-mode)
 
-     ;; Do not change the position of the cursor when scrolling pages
-     (setq scroll-preserve-screen-position t)
+       ;; All warning sounds and flash are invalid (note that the warning sound does not sound completely)
+       (setq ring-bell-function 'ignore)
 
-     ;; Suppress warnings for 'ad-handle-definition:'
-     (setq ad-redefinition-action 'accept)
+       ;; unable right-to-left language reordering
+       (setq-default bidi-display-reordering nil)
 
-     ;; Do not distinguish uppercase and lowercase letters on completion
-     (setq completion-ignore-case t)
-     (setq read-file-name-completion-ignore-case t)
+       ;; Point keeps its screen position when scroll
+       (setq scroll-preserve-screen-position :always)
 
-     ;; Copy with mouse drag
-     (setq mouse-drag-copy-region t)
-     ;; Do not make a backup filie like *.~
-     (setq make-backup-files nil)
-     ;; Do not use auto save
-     (setq auto-save-default nil)
-     ;; Do not create lock file
-     (setq create-lockfiles nil)
-     ;; Open symbolic link directly
-     (setq vc-follow-symlinks t)
+       ;; Turn Off warning sound screen flash
+       (setq visible-bell nil)
 
-     ;; Display file name in title bar: buffername-emacs-version
-     (setq frame-title-format "%b")
+       ;; Do not change the position of the cursor when scrolling pages
+       (setq scroll-preserve-screen-position t)
 
-     ;; Make it easy to see when it is the same name file
-     (leaf uniquify
-       :config
-       (setq uniquify-buffer-name-style 'post-forward-angle-brackets))
+       ;; Suppress warnings for 'ad-handle-definition:'
+       (setq ad-redefinition-action 'accept)
 
-     ;; contains many mode setting
-     (leaf generic-x :require t)
+       ;; Do not distinguish uppercase and lowercase letters on completion
+       (setq completion-ignore-case t)
+       (setq read-file-name-completion-ignore-case t)
 
-     ;; Text-scale-adjust
-     (bind-key "s-z" 'text-scale-adjust)
+       ;; Copy with mouse drag
+       (setq mouse-drag-copy-region t)
+       ;; Do not make a backup filie like *.~
+       (setq make-backup-files nil)
+       ;; Do not use auto save
+       (setq auto-save-default nil)
+       ;; Do not create lock file
+       (setq create-lockfiles nil)
+       ;; Open symbolic link directly
+       (setq vc-follow-symlinks t)
 
-     ;; Run muhenkan same as C-g
-     (bind-key* "<muhenkan>" 'minibuffer-keyboard-quit ivy-minibuffer-map)
+       ;; Display file name in title bar: buffername-emacs-version
+       (setq frame-title-format "%b")
 
-     ;; xref-find-* key
-     (bind-key "C-," 'xref-find-references)
-     (bind-key "C-." 'xref-find-definitions)
+       ;; Make it easy to see when it is the same name file
+       (leaf uniquify
+	 :config
+	 (setq uniquify-buffer-name-style 'post-forward-angle-brackets))
 
-     ;; Use the X11 clipboard
-     (setq select-enable-clipboard  t)
-     (setq select-enable-primary  t)
-     (bind-key "M-w" 'clipboard-kill-ring-save)
-     (bind-key "C-w" 'my:clipboard-kill-region)
+       ;; contains many mode setting
+       (leaf generic-x :require t)
 
-     (defun my:clipboard-kill-region ()
-       "If the region is active, `clipboard-kill-region'.
+       ;; Text-scale-adjust
+       (bind-key "s-z" 'text-scale-adjust)
+
+       ;; Run muhenkan same as C-g
+       (bind-key* "<muhenkan>" 'minibuffer-keyboard-quit ivy-minibuffer-map)
+
+       ;; xref-find-* key
+       (bind-key "C-," 'xref-find-references)
+       (bind-key "C-." 'xref-find-definitions)
+
+       ;; Use the X11 clipboard
+       (setq select-enable-clipboard  t)
+       (setq select-enable-primary  t)
+       (bind-key "M-w" 'clipboard-kill-ring-save)
+       (bind-key "C-w" 'my:clipboard-kill-region)
+
+       (defun my:clipboard-kill-region ()
+	 "If the region is active, `clipboard-kill-region'.
 If the region is inactive, `backward-kill-word'."
-       (interactive)
-       (if (use-region-p)
-	   (clipboard-kill-region (region-beginning) (region-end))
-	 (backward-kill-word 1)))
+	 (interactive)
+	 (if (use-region-p)
+	     (clipboard-kill-region (region-beginning) (region-end))
+	   (backward-kill-word 1)))
 
-     ;; Exit Emacs with M-x exitle
-     (defalias 'exit 'save-buffers-kill-emacs)
+       ;; Exit Emacs with M-x exitle
+       (defalias 'exit 'save-buffers-kill-emacs)
 
-     ;; Input yes or no to y or n (even SPC OK instead of y)
-     (defalias 'yes-or-no-p 'y-or-n-p)
+       ;; Input yes or no to y or n (even SPC OK instead of y)
+       (defalias 'yes-or-no-p 'y-or-n-p)
 
-     ;; Set transparency (active inactive)
-     (add-to-list 'default-frame-alist '(alpha . (1.0 0.9)))
+       ;; Set transparency (active inactive)
+       (add-to-list 'default-frame-alist '(alpha . (1.0 0.9)))
 
-     ;; Set makefle mode
-     (add-to-list 'auto-mode-alist '("\\.mak\\'" . makefile-mode))
+       ;; Set makefle mode
+       (add-to-list 'auto-mode-alist '("\\.mak\\'" . makefile-mode))
 
-     ;; M-x info-emacs-manual (C-h r or F1+r)
-     (add-to-list 'Info-directory-list "~/Dropbox/emacs.d/info/")
-     (defun Info-find-node--info-ja (orig-fn filename &rest args)
-       "Info as ORIG-FN FILENAME ARGS."
-       (apply orig-fn
-	      (pcase filename
-		("emacs" "emacs-ja.info")
-		(_ filename))
-	      args))
-     (advice-add 'Info-find-node :around 'Info-find-node--info-ja)
+       ;; M-x info-emacs-manual (C-h r or F1+r)
+       (add-to-list 'Info-directory-list "~/Dropbox/emacs.d/info/")
+       (defun Info-find-node--info-ja (orig-fn filename &rest args)
+	 "Info as ORIG-FN FILENAME ARGS."
+	 (apply orig-fn
+		(pcase filename
+		  ("emacs" "emacs-ja.info")
+		  (_ filename))
+		args))
+       (advice-add 'Info-find-node :around 'Info-find-node--info-ja))
 
      )))
 

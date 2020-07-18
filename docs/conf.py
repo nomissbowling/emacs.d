13dbbd5cd289bf2ede335eb32378233484dabfec
minorugh <minorugh@gmail.com>
2020-07-18 13:26:23 +0900

    Fix

--- docs/conf.py
+++ docs/conf.py
@@ -47,10 +47,13 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-source_suffix = ['.rst', '.md']
+from recommonmark.parser import CommonMarkParser
+
 source_parsers = {
-   '.md': 'recommonmark.parser.CommonMarkParser',
+    '.md': CommonMarkParser,
 }
+
+source_suffix = ['.rst', '.md']
 
 from recommonmark.transform import AutoStructify
 def setup(app):

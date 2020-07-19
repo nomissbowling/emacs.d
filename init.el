a15fef823fbe5bdcf3aef55ead4212351cbe0421
minorugh <minorugh@gmail.com>
2020-07-19 19:58:13 +0900

    Add Quiet startup

--- init.el
+++ init.el
@@ -4,6 +4,14 @@
 ;;; Code:
 ;; (setq debug-on-error t)
 
+;; Quiet Startup
+(set-frame-parameter nil 'fullscreen 'maximized)
+(scroll-bar-mode 0)
+(tool-bar-mode 0)
+(setq inhibit-splash-screen t)
+(setq inhibit-startup-message t)
+
+
 (defvar default-file-name-handler-alist file-name-handler-alist)
 (setq file-name-handler-alist nil)
 (setq gc-cons-threshold 100000000)
@@ -36,13 +44,12 @@
   (leaf-keywords-init))
 
 
+
 (leaf init-loader :ensure t
   :init
   (setq load-prefer-newer t)
   (setq el-get-dir "~/.emacs.d/elisp")
-  (add-to-list 'load-path "~/Dropbox/emacs.d/conf")
-  (require 'init-startup)
-  (require 'init-dashboard)
+  (load "~/Dropbox/emacs.d/elisp/init-dashboard.el")
   :config
   (add-hook
    'emacs-startup-hook

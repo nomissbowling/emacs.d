02292b6e89b21877f5b0e9ba95e6818262970729
minorugh <minorugh@gmail.com>
2020-07-19 17:29:56 +0900

    Move resentf to 01_base.el

--- conf/init-startup.el
+++ conf/init-startup.el
@@ -17,27 +17,13 @@
 (prefer-coding-system 'utf-8)
 
 
-;; Recentf
-(leaf recentf
-  :hook (after-init-hook . recentf-mode)
-  :config
-  (setq recentf-save-file "~/.emacs.d/recentf"
-	recentf-max-saved-items 200
-	recentf-auto-cleanup 'never
-	recentf-exclud '("recentf" "COMMIT_EDITMSG\\" "bookmarks" "emacs\\．d" "\\.gitignore"
-			 "\\.\\(?:gz\\|gif\\|svg\\|png\\|jpe?g\\)$" "\\.howm" "^/tmp/" "^/scp:"
-			 (lambda (file) (file-in-directory-p file package-user-dir))))
-  (push (expand-file-name recentf-save-file) recentf-exclude))
-
-
 ;; migemo
 (leaf migemo :ensure t
   :when (executable-find "cmigemo")
   :hook (after-init-hook . migemo-init)
   :config
   (setq migemo-command (executable-find "cmigemo")
-	migemo-dictionary "/usr/share/cmigemo/utf-8/migemo-dict")
-  (autoload 'migemo-init "migemo" nil t))
+	migemo-dictionary "/usr/share/cmigemo/utf-8/migemo-dict"))
 
 
 ;; (with-eval-after-load "time"

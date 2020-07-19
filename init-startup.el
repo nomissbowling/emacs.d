10f02e71975454dd12b1490d8e5f384750098ebe
minorugh <minorugh@gmail.com>
2020-07-19 15:11:43 +0900

    Fix ad:emacs-init-time

--- init-startup.el
+++ init-startup.el
@@ -40,6 +40,20 @@
   (autoload 'migemo-init "migemo" nil t))
 
 
+;; (with-eval-after-load "time"
+;;   (defun ad:emacs-init-time ()
+;;     "Return a string giving the duration of the Emacs initialization."
+;;     (interactive)
+;;     (let ((str
+;;            (format "%.3f seconds"
+;;                    (float-time
+;;                     (time-subtract after-init-time before-init-time)))))
+;;       (if (called-interactively-p 'interactive)
+;;           (message "%s" str)
+;;         str)))
+;;   (advice-add 'emacs-init-time :override #'ad:emacs-init-time))
+
+
 (provide 'init-startup)
 
 ;; Local Variables:

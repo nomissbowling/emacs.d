9b2ae7c162507aa7d29d377fab41b302c2557a0a
minorugh <minorugh@gmail.com>
2020-07-20 12:59:41 +0900

    Rename

--- init-conf.el
+++ init-conf.el
@@ -1,4 +1,4 @@
-;;; init-ui.el   -*- lexical-binding: t -*-
+;;; init-conf.el   -*- lexical-binding: t -*-
 
 ;; an extensible emacs startup screen
 
@@ -41,7 +41,6 @@
 (add-hook
  'emacs-startup-hook
  (lambda ()
-   (load "user-browse-url.el")
    (load "user-dired.el")
    (load "user-template.el")))
 
@@ -145,56 +144,8 @@
     (funcall (local-key-binding "r"))))
 
 
-(leaf *user-browse-function
-  :init
-  (defun browse-calendar ()
-    "Open Google-calendar with chrome."
-    (interactive)
-    (browse-url "https://calendar.google.com/calendar/r"))
-
-  (defun browse-weather ()
-    "Open tenki.jp with chrome."
-    (interactive)
-    (browse-url "https://tenki.jp/week/6/31/"))
-
-  (defun browse-google-news ()
-    "Open Google-news with chrome."
-    (interactive)
-    (browse-url "https://news.google.com/topstories?hl=ja&gl=JP&ceid=JP:ja"))
-
-  (defun browse-pocket ()
-    "Open pocket with chrome."
-    (interactive)
-    (browse-url "https://getpocket.com/a/queue/"))
-
-  (defun browse-keep ()
-    "Open pocket with chrome."
-    (interactive)
-    (browse-url "https://keep.new/"))
-
-  (defun browse-homepage ()
-    "Open my homepage."
-    (interactive)
-    (browse-url "https://gospel-haiku.com/update/"))
-
-  (defun browse-gmail ()
-    "Open gmail with chrome."
-    (interactive)
-    (browse-url "https://mail.google.com/mail/"))
-
-  (defun browse-tweetdeck ()
-    "Open tweetdeck with chrome."
-    (interactive)
-    (browse-url "https://tweetdeck.twitter.com/"))
-
-  (defun browse-slack ()
-    "Open slack with chrome."
-    (interactive)
-    (browse-url "https://emacs-jp.slack.com/messages/C1B73BWPJ/")))
-
-
 ;; Local Variables:
 ;; no-byte-compile: t
 ;; End:
 
-;;; init-ui.el ends here
+;;; init-conf.el ends here

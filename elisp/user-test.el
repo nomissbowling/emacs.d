97e333b7bc86c7a34298e400d75f3448f05baaab
minorugh <minorugh@gmail.com>
2020-07-20 14:58:09 +0900

    New file

---
+++ elisp/user-test.el
@@ -0,0 +1,19 @@
+;;; user-test.el -*- lexical-binding: t -*-
+;; open user directory
+
+;;; Code:
+;; (setq debug-on-error t)
+
+;; Write a trial settings
+
+
+
+
+(provide 'user-test)
+
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
+
+;;; user-test.el ends here
+

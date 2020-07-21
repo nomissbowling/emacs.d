a14c650bd437ea4ce5f2999a923ec7e863626804
minorugh <minorugh@gmail.com>
2020-07-21 18:39:54 +0900

    Add emacs-startup-hook

--- inits/07_ui.el
+++ inits/07_ui.el
@@ -11,7 +11,7 @@
 
 (leaf doom-modeline :ensure t
   :config
-  (doom-modeline-mode)
+  (add-hook 'emacs-startup-hook 'doom-modeline-mode)
   (line-number-mode 0)
   (column-number-mode 0)
   (setq doom-modeline-buffer-file-name-style 'truncate-with-project
@@ -25,39 +25,40 @@
     :config
     (nyan-mode)
     (setq nyan-cat-face-number 4
-	  nyan-animate-nyancat t))
+  	  nyan-animate-nyancat t)))
 
-  (leaf all-the-icons :ensure t
+
+(leaf all-the-icons :ensure t
+  :config
+  (setq all-the-icons-scale-factor 1.0)
+  :init
+  (leaf all-the-icons-dired :ensure t
+    :hook (dired-mode-hook . all-the-icons-dired-mode))
+  (leaf all-the-icons-ivy-rich :ensure t
     :config
-    (setq all-the-icons-scale-factor 1.0)
-    :init
-    (leaf all-the-icons-dired :ensure t
-      :hook (dired-mode-hook . all-the-icons-dired-mode))
-    (leaf all-the-icons-ivy-rich :ensure t
-      :config
-      (all-the-icons-ivy-rich-mode)))
+    (all-the-icons-ivy-rich-mode)))
 
 
-  (leaf darkroom :ensure t
-    :bind ("<f12>" . my:darkroom-mode-in)
-    :config
-    (defun my:darkroom-mode-in ()
-      "Darkroom mode in."
-      (interactive)
-      (display-line-numbers-mode 0)
-      (diff-hl-mode 0)
-      (flymake-mode 0)
-      (setq line-spacing 0.4)
-      (darkroom-mode 1)
-      (bind-key "<f12>" 'my:darkroom-mode-out darkroom-mode-map))
-    (defun my:darkroom-mode-out ()
-      "Darkroom mode out."
-      (interactive)
-      (darkroom-mode 0)
-      (flymake-mode 1)
-      (diff-hl-mode 1)
-      (setq line-spacing 0.1)
-      (display-line-numbers-mode 1))))
+(leaf darkroom :ensure t
+  :bind ("<f12>" . my:darkroom-mode-in)
+  :config
+  (defun my:darkroom-mode-in ()
+    "Darkroom mode in."
+    (interactive)
+    (display-line-numbers-mode 0)
+    (diff-hl-mode 0)
+    (flymake-mode 0)
+    (setq line-spacing 0.4)
+    (darkroom-mode 1)
+    (bind-key "<f12>" 'my:darkroom-mode-out darkroom-mode-map))
+  (defun my:darkroom-mode-out ()
+    "Darkroom mode out."
+    (interactive)
+    (darkroom-mode 0)
+    (flymake-mode 1)
+    (diff-hl-mode 1)
+    (setq line-spacing 0.1)
+    (display-line-numbers-mode 1)))
 
 
 ;; Local Variables:

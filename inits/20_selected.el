af5b72e666ecb8d7032579969e3240ab6f55d2bd
minorugh <minorugh@gmail.com>
2020-07-25 19:50:32 +0900

    Refactor

--- inits/20_selected.el
+++ inits/20_selected.el
@@ -34,91 +34,86 @@
     (hydra-selected
      (:color red :hint nil)
      "
- 🔍 _t_ranslate  _g_oogle  _e_ijiro  _w_eblio  _k_obun  _r_uigo  _l_ist  mozc:_._"
+ 🔍 _t_ranslate  _e_ijiro  _w_eblio  _k_obun  _r_uigo  _g_oogle  _l_ist"
      ("t" my:translate)
      ("e" my:eijiro)
      ("w" my:weblio)
      ("k" my:kobun)
      ("r" my:ruigo)
-     ("p" my:postal)
-     ("m" my:g-map)
      ("g" my:google)
-     ("." my:mozc-word-regist)
-     (";" comment-dwim)
-     ("l" cunsel-selected)
-     ("c" clipboard-kill-ring-save)))
+     ("l" cunsel-selected))))
 
 
-  (leaf  *control-mozc-when-region-seleceted
-    :init
-    (defun my-activate-selected ()
-      (selected-global-mode 1)
-      (selected--on) ;; must call expclitly here
-      (remove-hook 'activate-mark-hook #'my-activate-selected))
-    (add-hook 'activate-mark-hook #'my-activate-selected)
-    (defun my:ime-on ()
-      (interactive)
-      (when (null current-input-method) (toggle-input-method)))
-    (defun my:ime-off ()
-      (interactive)
-      (inactivate-input-method))
-    ;; activate-mark-hook
-    (add-hook
-     'activate-mark-hook
-     #'(lambda ()
-	 (setq my:ime-flag current-input-method) (my:ime-off)))
-    (add-hook
-     'deactivate-mark-hook
-     #'(lambda ()
-	 (unless (null my:ime-flag) (my:ime-on)))))
+(leaf  *control-mozc-when-region-seleceted
+  :init
+  (defun my-activate-selected ()
+    (selected-global-mode 1)
+    (selected--on) ;; must call expclitly here
+    (remove-hook 'activate-mark-hook #'my-activate-selected))
+  (add-hook 'activate-mark-hook #'my-activate-selected)
+  (defun my:ime-on ()
+    (interactive)
+    (when (null current-input-method) (toggle-input-method)))
+  (defun my:ime-off ()
+    (interactive)
+    (inactivate-input-method))
+  ;; activate-mark-hook
+  (add-hook
+   'activate-mark-hook
+   #'(lambda ()
+       (setq my:ime-flag current-input-method) (my:ime-off)))
+  (add-hook
+   'deactivate-mark-hook
+   #'(lambda ()
+       (unless (null my:ime-flag) (my:ime-on)))))
 
 
-  (leaf *user-word-search-functions
-    :init
-    ;; Weblio
-    (defun my:weblio (str)
-      (interactive (list (region-or-read-string nil)))
-      (browse-url (format "http://www.weblio.jp/content/%s"
-			  (upcase (url-hexify-string str)))))
-    ;; Kobun
-    (defun my:kobun (str)
-      (interactive (list (region-or-read-string nil)))
-      (browse-url (format "https://kobun.weblio.jp/content/%s"
-			  (upcase (url-hexify-string str)))))
-    ;; Ruigo
-    (defun my:ruigo (str)
-      (interactive (list (region-or-read-string nil)))
-      (browse-url (format "https://thesaurus.weblio.jp/content/%s"
-			  (upcase (url-hexify-string str)))))
-    ;; Eijiro
-    (defun my:eijiro (str)
-      (interactive (list (region-or-read-string nil)))
-      (browse-url (format "http://eow.alc.co.jp/%s/UTF-8/"
-			  (upcase (url-hexify-string str)))))
-    ;; Postal code
-    (defun my:postal (str)
-      (interactive (list (region-or-read-string nil)))
-      (browse-url (format "https://postcode.goo.ne.jp/search/q/%s"
-			  (upcase (url-hexify-string str)))))
-    ;; Google
-    (defun my:google (str)
-      (interactive (list (region-or-read-string nil)))
-      (browse-url (format "http://www.google.com/search?hl=ja&q=%s"
-			  (upcase (url-hexify-string str)))))
-    ;; Google map
-    (defun my:g-map (str)
-      (interactive (list (region-or-read-string nil)))
-      (browse-url (format "http://maps.google.co.jp/maps?hl=ja&q=%s"
-			  (upcase (url-hexify-string str)))))
+(leaf *user-word-search-functions
+  :init
+  ;; Weblio
+  (defun my:weblio (str)
+    (interactive (list (region-or-read-string nil)))
+    (browse-url (format "http://www.weblio.jp/content/%s"
+			(upcase (url-hexify-string str)))))
+  ;; Kobun
+  (defun my:kobun (str)
+    (interactive (list (region-or-read-string nil)))
+    (browse-url (format "https://kobun.weblio.jp/content/%s"
+			(upcase (url-hexify-string str)))))
+  ;; Ruigo
+  (defun my:ruigo (str)
+    (interactive (list (region-or-read-string nil)))
+    (browse-url (format "https://thesaurus.weblio.jp/content/%s"
+			(upcase (url-hexify-string str)))))
+  ;; Eijiro
+  (defun my:eijiro (str)
+    (interactive (list (region-or-read-string nil)))
+    (browse-url (format "http://eow.alc.co.jp/%s/UTF-8/"
+			(upcase (url-hexify-string str)))))
+  ;; Postal code
+  (defun my:postal (str)
+    (interactive (list (region-or-read-string nil)))
+    (browse-url (format "https://postcode.goo.ne.jp/search/q/%s"
+			(upcase (url-hexify-string str)))))
+  ;; Google
+  (defun my:google (str)
+    (interactive (list (region-or-read-string nil)))
+    (browse-url (format "http://www.google.com/search?hl=ja&q=%s"
+			(upcase (url-hexify-string str)))))
+  ;; Google map
+  (defun my:g-map (str)
+    (interactive (list (region-or-read-string nil)))
+    (browse-url (format "http://maps.google.co.jp/maps?hl=ja&q=%s"
+			(upcase (url-hexify-string str)))))
 
-    (defun region-or-read-string (prompt &optional initial history default inherit)
-      "If region is specified, get that string, otherwise call `read-string'."
-      (if (not (region-active-p))
-	  (read-string prompt initial history default inherit)
-	(prog1
-	    (buffer-substring-no-properties (region-beginning) (region-end))
-	  (deactivate-mark)
-	  (message ""))))))
+  (defun region-or-read-string (prompt &optional initial history default inherit)
+    "If region is specified, get that string, otherwise call `read-string'."
+    (if (not (region-active-p))
+	(read-string prompt initial history default inherit)
+      (prog1
+	  (buffer-substring-no-properties (region-beginning) (region-end))
+	(deactivate-mark)
+	(message ""))))))
 
 
 ;; Local Variables:

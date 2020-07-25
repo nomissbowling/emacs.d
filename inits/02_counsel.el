92126f843c792118197ba752890c5aeea719cb14
minorugh <minorugh@gmail.com>
2020-07-25 16:59:59 +0900

    Fix key bind

--- inits/02_counsel.el
+++ inits/02_counsel.el
@@ -61,7 +61,7 @@ If the region isn't selected, `swiper' with migemo."
   (setq ivy-use-virtual-buffers t)
   (setq ivy-use-selectable-prompt t)
   (setq enable-recursive-minibuffers t)
-  (bind-key "s-s" 'swiper-thing-at-point)
+  (bind-key "M-s" 'swiper-thing-at-point)
   (bind-key "C-s" 'swiper-migemo-or-region)
   (bind-key "C-:" 'counsel-switch-buffer)
   (bind-key "C-x C-b" 'counsel-switch-buffer)

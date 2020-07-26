835213304025ed35bb57bdf669121eb26d461ae9
minorugh <minorugh@gmail.com>
2020-07-26 11:22:06 +0900

    Fix ps-print

--- inits/30_utils.el
+++ inits/30_utils.el
@@ -27,9 +27,11 @@
   (defalias 'my:github-show 'browse-at-remote))
 
 
-;; ps-print-buffer
-(leaf ps-print :ensure nil
+;; ps-print
+(leaf ps-mule
   :config
+  (setq ps-multibyte-buffer 'non-latin-printer)
+  (defalias 'ps-mule-header-string-charsets 'ignore)
   (setq ps-paper-type 'a4)
   (setq ps-font-size 9)
   (setq ps-printer-name nil)
@@ -39,91 +41,97 @@
   (setq ps-print-footer nil))
 
 
-;; pdf out from emacs
-(setq my:pdfout-command-format "nkf -e | e2ps -a4 -p -nh | ps2pdf - %s")
-;; (setq my:pdfout-command-format "nkf -e | e2ps -a4 -p -nh | lpr")
-(defun my:pdfout-buffer ()
-  "PDF out from buffer."
-  (interactive)
-  (my:pdfout-region (point-min) (point-max)))
-(defun my:pdfout-region (begin end)
-  "PDF out from BEGIN to END of region."
-  (interactive "r")
-  ;; (shell-command-on-region begin end my:pdfout-command-format)))
-  (shell-command-on-region begin end (format my:pdfout-command-format
-					     (concat (read-from-minibuffer "File name:") ".pdf"))))
+(leaf *pdfout-from-emacs
+  :init
+  (defun pdfout-select ()
+    "PDF out select menu."
+    (interactive)
+    (counsel-M-x "^my:pdfout "))
+  (setq my:pdfout-command-format "nkf -e | e2ps -a4 -p -nh | ps2pdf - %s")
+  (defun my:pdfout-buffer ()
+    "PDF out from buffer."
+    (interactive)
+    (my:pdfout-region (point-min) (point-max)))
+  (defun my:pdfout-region (begin end)
+    "PDF out from BEGIN to END of region."
+    (interactive "r")
+    ;; (shell-command-on-region begin end my:pdfout-command-format)))
+    (shell-command-on-region begin end (format my:pdfout-command-format
+					       (concat (read-from-minibuffer "File name:") ".pdf")))))
 
 
-;; user-utils-function
-(defun filer-current-dir-open ()
-  "Open filer in current dir."
-  (interactive)
-  (compile (concat "Thunar " default-directory)))
-(bind-key "<f3>" 'filer-current-dir-open)
+(leaf *user-functions-utils
+  :init
+  (defun filer-current-dir-open ()
+    "Open filer in current dir."
+    (interactive)
+    (compile (concat "Thunar " default-directory)))
+  (bind-key "<f3>" 'filer-current-dir-open)
 
-(defun term-current-dir-open ()
-  "Open terminal application in current dir."
-  (interactive)
-  (let ((dir (directory-file-name default-directory)))
-    (compile (concat "gnome-terminal --working-directory " dir))))
-(bind-key "<f4>" 'term-current-dir-open)
+  (defun term-current-dir-open ()
+    "Open terminal application in current dir."
+    (interactive)
+    (let ((dir (directory-file-name default-directory)))
+      (compile (concat "gnome-terminal --working-directory " dir))))
+  (bind-key "<f4>" 'term-current-dir-open)
 
-(defun my:delete-file-if-no-contents ()
-  "Automatic deletion for empty files (Valid in all modes)."
-  (when (and (buffer-file-name (current-buffer))
-	     (= (point-min) (point-max)))
-    (delete-file
-     (buffer-file-name (current-buffer)))))
-(if (not (memq 'my:delete-file-if-no-contents after-save-hook))
-    (setq after-save-hook
-	  (cons 'my:delete-file-if-no-contents after-save-hook)))
+  (defun my:delete-file-if-no-contents ()
+    "Automatic deletion for empty files (Valid in all modes)."
+    (when (and (buffer-file-name (current-buffer))
+	       (= (point-min) (point-max)))
+      (delete-file
+       (buffer-file-name (current-buffer)))))
+  (if (not (memq 'my:delete-file-if-no-contents after-save-hook))
+      (setq after-save-hook
+	    (cons 'my:delete-file-if-no-contents after-save-hook))))
 
 
-;; user-browse-url-function
-(defun browse-calendar ()
-  "Open Google-calendar with chrome."
-  (interactive)
-  (browse-url "https://calendar.google.com/calendar/r"))
+(leaf *user-function-for-browse-url
+  :init
+  (defun browse-calendar ()
+    "Open Google-calendar with chrome."
+    (interactive)
+    (browse-url "https://calendar.google.com/calendar/r"))
 
-(defun browse-weather ()
-  "Open tenki.jp with chrome."
-  (interactive)
-  (browse-url "https://tenki.jp/week/6/31/"))
+  (defun browse-weather ()
+    "Open tenki.jp with chrome."
+    (interactive)
+    (browse-url "https://tenki.jp/week/6/31/"))
 
-(defun browse-google-news ()
-  "Open Google-news with chrome."
-  (interactive)
-  (browse-url "https://news.google.com/topstories?hl=ja&gl=JP&ceid=JP:ja"))
+  (defun browse-google-news ()
+    "Open Google-news with chrome."
+    (interactive)
+    (browse-url "https://news.google.com/topstories?hl=ja&gl=JP&ceid=JP:ja"))
 
-(defun browse-pocket ()
-  "Open pocket with chrome."
-  (interactive)
-  (browse-url "https://getpocket.com/a/queue/"))
+  (defun browse-pocket ()
+    "Open pocket with chrome."
+    (interactive)
+    (browse-url "https://getpocket.com/a/queue/"))
 
-(defun browse-keep ()
-  "Open pocket with chrome."
-  (interactive)
-  (browse-url "https://keep.new/"))
+  (defun browse-keep ()
+    "Open pocket with chrome."
+    (interactive)
+    (browse-url "https://keep.new/"))
 
-(defun browse-homepage ()
-  "Open my homepage with crome."
-  (interactive)
-  (browse-url "https://gospel-haiku.com/update/"))
+  (defun browse-homepage ()
+    "Open my homepage with crome."
+    (interactive)
+    (browse-url "https://gospel-haiku.com/update/"))
 
-(defun browse-gmail ()
-  "Open gmail with chrome."
-  (interactive)
-  (browse-url "https://mail.google.com/mail/"))
+  (defun browse-gmail ()
+    "Open gmail with chrome."
+    (interactive)
+    (browse-url "https://mail.google.com/mail/"))
 
-(defun browse-tweetdeck ()
-  "Open tweetdeck with chrome."
-  (interactive)
-  (browse-url "https://tweetdeck.twitter.com/"))
+  (defun browse-tweetdeck ()
+    "Open tweetdeck with chrome."
+    (interactive)
+    (browse-url "https://tweetdeck.twitter.com/"))
 
-(defun browse-slack ()
-  "Open slack with chrome."
-  (interactive)
-  (browse-url "https://emacs-jp.slack.com/messages/C1B73BWPJ/"))
+  (defun browse-slack ()
+    "Open slack with chrome."
+    (interactive)
+    (browse-url "https://emacs-jp.slack.com/messages/C1B73BWPJ/")))
 
 
 ;; Local Variables:

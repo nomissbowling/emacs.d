97ffb888d78168d8ae9c45e1093e5d27bc98fd78
minorugh <minorugh@gmail.com>
2020-07-26 11:22:20 +0900

    Refactor

--- inits/40_direx.el
+++ inits/40_direx.el
@@ -4,26 +4,6 @@
 ;;; Code:
 ;; (setq debug-on-error t)
 
-;; https://blog.shibayu36.org/entry/2013/02/12/191459
-(defun direx:jump-to-project-directory ()
-  "If in project, launch direx-project otherwise start direx."
-  (interactive)
-  (let ((result (ignore-errors (direx-project:jump-to-project-root-other-window) t)))
-    (unless result (direx:jump-to-directory-other-window))))
-
-(defun direx:open-file ()
-  "In direx, open the file in associated application."
-  (interactive)
-  (let* ((item (direx:item-at-point!))
-	 (file (direx:item-tree item))
-	 (file-full-name (direx:file-full-name file)))
-    (unless (getenv "WSLENV")
-      (call-process "xdg-open" nil 0 nil file-full-name))
-    ;; use wsl-utils:https://github.com/smzht/wsl-utils
-    (when (getenv "WSLENV")
-      (call-process "wslstart" nil 0 nil file-full-name))))
-
-
 (leaf direx :ensure t
   :config
   (setq direx:leaf-icon "  " direx:open-icon "📂" direx:closed-icon "📁")
@@ -35,6 +15,28 @@
     (bind-key "o" 'direx:open-file direx:direx-mode-map)
     (bind-key "SPC" 'direx:find-item-other-window direx:direx-mode-map)
     (bind-key "<f10>" 'quit-window direx:direx-mode-map)))
+
+
+(leaf *extension-function-for-direx
+  :init
+  ;; https://blog.shibayu36.org/entry/2013/02/12/191459
+  (defun direx:jump-to-project-directory ()
+    "If in project, launch direx-project otherwise start direx."
+    (interactive)
+    (let ((result (ignore-errors (direx-project:jump-to-project-root-other-window) t)))
+      (unless result (direx:jump-to-directory-other-window))))
+
+  (defun direx:open-file ()
+    "In direx, open the file in associated application."
+    (interactive)
+    (let* ((item (direx:item-at-point!))
+	   (file (direx:item-tree item))
+	   (file-full-name (direx:file-full-name file)))
+      (unless (getenv "WSLENV")
+	(call-process "xdg-open" nil 0 nil file-full-name))
+      ;; use wsl-utils:https://github.com/smzht/wsl-utils
+      (when (getenv "WSLENV")
+	(call-process "wslstart" nil 0 nil file-full-name)))))
 
 
 ;; Local Variables:

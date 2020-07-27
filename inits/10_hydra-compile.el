49c48cbc05cb8e55585e7e0618a2548fc33796de
minorugh <minorugh@gmail.com>
2020-07-27 11:01:47 +0900

    Fix

--- inits/10_hydra-compile.el
+++ inits/10_hydra-compile.el
@@ -4,40 +4,39 @@
 ;;; Code:
 ;; (setq debug-on-error t)
 
-(eval-when-compile
-  (leaf *user-make-function
-    :hydra
-    (hydra-compile
-     (:color red :hint nil)
-     "
+(leaf *user-make-function
+  :hydra
+  (hydra-compile
+   (:color red :hint nil)
+   "
    🗿 Compile: make:_k_  _u_pftp  _m_ove  _b_klog  _g_it  _c_lean  _e_rror 🐾 "
-     ("k" my:make-k)
-     ("u" my:make-upftp)
-     ("m" my:make-move)
-     ("b" my:make-bklog)
-     ("g" my:make-git)
-     ("c" my:make-clean)
-     ("e" next-error)
-     ("<muhenkan>" nil))
-    :config
-    ;; https://gist.github.com/EricCrosson/fa41233f327403ea2a5a
-    (defun close-compile-window-if-successful (buffer string)
-      "Close a compilation window if succeeded without warnings."
-      (when (and
-	     (string-match "compilation" (buffer-name buffer))
-	     (string-match "finished" string)
-	     (not
-	      (with-current-buffer buffer
-		(search-forward "warning" nil t))))
-	(run-with-timer 1 nil
-			(lambda ()
-			  (delete-other-windows)))))
-    (add-hook 'compilation-finish-functions 'close-compile-window-if-successful)
+   ("k" my:make-k)
+   ("u" my:make-upftp)
+   ("m" my:make-move)
+   ("b" my:make-bklog)
+   ("g" my:make-git)
+   ("c" my:make-clean)
+   ("e" next-error)
+   ("<muhenkan>" nil))
+  :config
+  ;; https://gist.github.com/EricCrosson/fa41233f327403ea2a5a
+  (defun close-compile-window-if-successful (buffer string)
+    "Close a compilation window if succeeded without warnings."
+    (when (and
+	   (string-match "compilation" (buffer-name buffer))
+	   (string-match "finished" string)
+	   (not
+	    (with-current-buffer buffer
+	      (search-forward "warning" nil t))))
+      (run-with-timer 1 nil
+		      (lambda ()
+			(delete-other-windows)))))
+  (add-hook 'compilation-finish-functions 'close-compile-window-if-successful)
 
-    ;; Turn off 'Suspicious line XXX of Makefile.' makefile warning
-    (add-hook 'makefile-mode-hook
-	      (lambda ()
-		(fset 'makefile-warn-suspicious-lines 'ignore)))))
+  ;; Turn off 'Suspicious line XXX of Makefile.' makefile warning
+  (add-hook 'makefile-mode-hook
+	    (lambda ()
+	      (fset 'makefile-warn-suspicious-lines 'ignore))))
 
 
   (leaf *user-compile-command

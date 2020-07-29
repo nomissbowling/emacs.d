6d0059392317ebb67e38a182105cf209434c4640
minorugh <minorugh@gmail.com>
2020-07-29 14:07:30 +0900

    Fix key bind

--- inits/50_view-mode.el
+++ inits/50_view-mode.el
@@ -23,8 +23,8 @@
    (define-key view-mode-map "c" 'avy-goto-char-2)
    (define-key view-mode-map "b" 'scroll-down-command)
    (define-key view-mode-map "D" 'my:view-kill-whole-line)
-   (define-key view-mode-map "u" 'my:view-undo)
-   (define-key view-mode-map "X" 'my:view-del-char)
+   ;; (define-key view-mode-map "u" 'my:view-undo)
+   ;; (define-key view-mode-map "X" 'my:view-del-char)
    (define-key view-mode-map "w" 'my:view-forward-word+1)
    (define-key view-mode-map "W" 'backward-word)
    (define-key view-mode-map "s" 'swiper-or-region)
@@ -34,11 +34,16 @@
    (define-key view-mode-map ";" 'recenter-top-bottom)
    (define-key view-mode-map "t" 'direx:jump-to-project-directory)
    (define-key view-mode-map "o" 'other-window-or-split)
+   (define-key view-mode-map ">" 'text-scale-increase)
+   (define-key view-mode-map "<" 'text-scale-decrease)
+   (define-key view-mode-map "-" '(text-scale-set 0))
    (define-key view-mode-map "0" 'delete-window)
    (define-key view-mode-map "1" 'delete-other-windows)
-   (define-key view-mode-map "2" 'split-window-below)
-   (define-key view-mode-map "3" 'split-window-right)
-   (define-key view-mode-map "x" 'window-toggle-division)
+   (define-key view-mode-map "d" 'vc-diff)
+   (define-key view-mode-map "n" 'diff-hl-next-hunk)
+   (define-key view-mode-map "p" 'diff-hl-previous-hunk)
+   (define-key view-mode-map "s" 'swiper-or-region)
+   (define-key view-mode-map "S" 'counsel-switch-buffer)
    (define-key view-mode-map "." 'hydra-view-mode/body)))
 
 
@@ -78,11 +83,11 @@
 (defhydra hydra-view-mode (:color pink :hint nil)
   "
  🐳 View-mode Function:
-  _SPC_: next page       _a_: top of line      _u_: view undo          _o_: other-window     _0_: delete-window
-    _b_: prev page       _e_: end of line      _w_: forward word       _t_: direx:tree       _1_: del-other-windows
-    _g_: page top        _l_: goto line        _W_: backward word      _i_: view exit        _2_: split--below
-    _G_: page end        _D_: delete line      _[_: forward pair       _q_: view quit        _3_: split-right
-    _;_: top-bottom      _X_: delete char      _]_: backward pair      _._: close            _x_: toggle-division"
+  _SPC_: next page       _a_: top of line       _<__-__>_: text-scale       _o_: other-window     _0_: delete-window
+    _b_: prev page       _e_: end of line       _w_: forward word^^^^       _t_: direx:tree       _1_: del-other-windows
+    _g_: page top        _l_: goto line         _W_: backward word^^^^      _:_: view exit        _d_: vc-diff
+    _G_: page end        _n_: diff-next-hunk    _[_: forward pair^^^^       _v_: view mode        _s_: swiper
+    _;_: recenter        _p_: diff-prev-hunk    _]_: backward pair^^^^      _._: close            _S_: switch-buffer"
   ;; Move page
   ("SPC" scroll-up-command)
   ("b" scroll-down-command)
@@ -94,23 +99,27 @@
   ("e" seq-end)
   ("w" my:view-forward-word+1)
   ("W" backward-word)
-  ("D" my:view-kill-whole-line)
-  ("X" my:view-del-char)
-  ("u" my:view-undo)
+  ;; ("D" my:view-kill-whole-line)
+  ;; ("X" my:view-del-char)
+  ;; ("u" my:view-undo)
   ;; Misc
-  ("i" View-exit :exit t)
-  ("q" View-quit :exit t)
   (":" View-exit :exit t)
+  ("v" view-mode)
   ("[" forward-list)
-  ("]" backward-lis)
+  ("]" backward-list)
   ("l" avy-goto-line)
   (";" recenter-top-bottom)
   ;;window
+  (">" text-scale-increase)
+  ("<" text-scale-decrease)
+  ("-" (text-scale-set 0))
   ("0" delete-window)
   ("1" delete-other-windows)
-  ("2" split-window-below)
-  ("3" split-window-right)
-  ("x" window-toggle-division)
+  ("d" vc-diff :exit t)
+  ("n" diff-hl-next-hunk)
+  ("p" diff-hl-previous-hunk)
+  ("s" swiper-or-region)
+  ("S" counsel-switch-buffer)
   ;; Others
   ("o" other-window-or-split :exit t)
   ("t" direx:jump-to-project-directory)

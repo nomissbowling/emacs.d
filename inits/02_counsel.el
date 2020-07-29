7b198504945a30f48b561900928aa29a4832c863
minorugh <minorugh@gmail.com>
2020-07-29 09:10:21 +0900

    Fix key bind

--- inits/02_counsel.el
+++ inits/02_counsel.el
@@ -21,6 +21,7 @@
   (bind-key "M-x" 'counsel-M-x)
   (bind-key "M-y" 'counsel-yank-pop)
   (bind-key "s-a" 'counsel-linux-app)
+  (bind-key "s-c" 'counsel-mark-ring)
   (bind-key "C-x C-f" 'counsel-find-file)
   (bind-key "C-c k" 'counsel-ag)
   (bind-key "C-c w" 'counsel-rg)

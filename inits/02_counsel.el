49e6d2cc2cb73a10ec6427c3f1eb9521037f38d2
minorugh <minorugh@gmail.com>
2020-07-30 13:33:54 +0900

    Refactor

--- inits/02_counsel.el
+++ inits/02_counsel.el
@@ -9,7 +9,7 @@
   (ivy-mode-hook . counsel-mode)
   (css-mode-hook . counsel-css-imenu-setup)
   :config
-  (ivy-mode)
+  (ivy-mode 1)
   (setq ivy-use-virtual-buffers t)
   (setq ivy-use-selectable-prompt t)
   (setq enable-recursive-minibuffers t)
@@ -48,51 +48,49 @@
     :hook (ivy-mode-hook . ivy-rich-mode)))
 
 
-(eval-when-compile
-  (leaf *user-function-for-counsel
-    :init
-    (defun swiper-or-region ()
-      "If region is selected, `swiper' with the keyword selected in region.
+(leaf *user-function-for-counsel
+  :init
+  (defun swiper-or-region ()
+    "If region is selected, `swiper' with the keyword selected in region.
 If the region isn't selected, `swiper' with migemo."
-      (interactive)
-      (if (not (use-region-p))
-	  (swiper)
-	(swiper-thing-at-point)))
+    (interactive)
+    (if (not (use-region-p))
+	(swiper)
+      (swiper-thing-at-point)))
 
-    (defun my:ivy-migemo-re-builder (str)
-      "Own ivy-migemo-re-build for swiper."
-      (let* ((sep " \\|\\^\\|\\.\\|\\*")
-	     (splitted (--map (s-join "" it)
-			      (--partition-by (s-matches-p " \\|\\^\\|\\.\\|\\*" it)
-					      (s-split "" str t)))))
-	(s-join "" (--map (cond ((s-equals? it " ") ".*?")
-				((s-matches? sep it) it)
-				(t (migemo-get-pattern it)))
-			  splitted))))
+  (defun my:ivy-migemo-re-builder (str)
+    "Own ivy-migemo-re-build for swiper."
+    (let* ((sep " \\|\\^\\|\\.\\|\\*")
+	   (splitted (--map (s-join "" it)
+			    (--partition-by (s-matches-p " \\|\\^\\|\\.\\|\\*" it)
+					    (s-split "" str t)))))
+      (s-join "" (--map (cond ((s-equals? it " ") ".*?")
+			      ((s-matches? sep it) it)
+			      (t (migemo-get-pattern it)))
+			splitted))))
 
-    (setq ivy-re-builders-alist '((t . ivy--regex-plus)
-				  (counsel-rg . my:ivy-migemo-re-builder)
-				  (swiper . my:ivy-migemo-re-builder)))
+  (setq ivy-re-builders-alist '((t . ivy--regex-plus)
+				(counsel-rg . my:ivy-migemo-re-builder)
+				(swiper . my:ivy-migemo-re-builder)))
 
-    (defun my:ivy-format-function-arrow (cands)
-      "Transform into a string for minibuffer."
-      (ivy--format-function-generic
-       (lambda (str)
-	 (concat (if (display-graphic-p)
-		     (all-the-icons-octicon "chevron-right" :height 0.8 :v-adjust -0.05)
-		   ">")
-		 (propertize " " 'display `(space :align-to 2))
-		 (ivy--add-face str 'ivy-current-match)))
-       (lambda (str)
-	 (concat (propertize " " 'display `(space :align-to 2)) str))
-       cands
-       "\n"))
+  (defun my:ivy-format-function-arrow (cands)
+    "Transform into a string for minibuffer."
+    (ivy--format-function-generic
+     (lambda (str)
+       (concat (if (display-graphic-p)
+		   (all-the-icons-octicon "chevron-right" :height 0.8 :v-adjust -0.05)
+		 ">")
+	       (propertize " " 'display `(space :align-to 2))
+	       (ivy--add-face str 'ivy-current-match)))
+     (lambda (str)
+       (concat (propertize " " 'display `(space :align-to 2)) str))
+     cands
+     "\n"))
 
-    (defun select-counsel-command ()
-      "Narrow the only counsel-command in `M-x'."
-      (interactive)
-      (counsel-M-x "^counsel "))))
-
+  (defun select-counsel-command ()
+    "Narrow the only counsel-command in `M-x'."
+    (interactive)
+    (counsel-M-x "^counsel ")))
 
 
 (leaf counsel-tramp :ensure t

6bf0bfe3800bb8fb114f6c4efaaf13c58077b9c7
minorugh <minorugh@gmail.com>
2020-07-31 22:43:41 +0900

    Refactor

--- inits/05_mozc.el
+++ inits/05_mozc.el
@@ -30,16 +30,16 @@
   :preface
   (leaf mozc-cursor-color
     :el-get iRi-E/mozc-el-extensions
+    :require t
     :config
-    (when (require 'mozc-cursor-color nil t)
-      (setq mozc-cursor-color-alist
-	    '((direct . "#BD93F9")
-	      (read-only . "#84A0C6")
-	      (hiragana . "#CC3333")))))
+    (setq mozc-cursor-color-alist
+	  '((direct . "#BD93F9")
+	    (read-only . "#84A0C6")
+	    (hiragana . "#CC3333"))))
   (leaf mozc-cand-posframe :ensure t
+    :require t
     :config
-    (when (require 'mozc-cand-posframe nil t)
-      (setq mozc-candidate-style 'posframe))))
+    (setq mozc-candidate-style 'posframe)))
 
 
 (leaf *user-mozc-tool

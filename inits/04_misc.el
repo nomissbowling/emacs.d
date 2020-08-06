676c74853cfcb94400ad6209d4aff0318b739992
minorugh <minorugh@gmail.com>
2020-08-06 21:44:17 +0900

    Refactor

--- inits/04_misc.el
+++ inits/04_misc.el
@@ -19,7 +19,8 @@
   (push 'er/mark-outside-pairs er/try-expand-list))
 
 
-(leaf cool-copy :el-get blue0513/cool-copy.el
+(leaf cool-copy
+  :el-get blue0513/cool-copy.el
   :bind ("s-c" . cool-copy)
   :config
   (setq cool-copy-show 'posframe))

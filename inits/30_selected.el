6d0befe009ede841ea237d5fb61b8497e2debde3
minorugh <minorugh@gmail.com>
2020-08-20 10:36:30 +0900

    Fix key bind

--- inits/30_selected.el
+++ inits/30_selected.el
@@ -13,13 +13,10 @@
     (bind-key "v" 'describe-variable selected-keymap)
     (bind-key "c" 'clipboard-kill-ring-save selected-keymap)
     (bind-key "i" 'iedit-mode selected-keymap)
-    (bind-key "s" 'swiper-thing-at-point selected-keymap)
     (bind-key "d" 'my:koujien selected-keymap)
     (bind-key "e" 'my:eijiro selected-keymap)
     (bind-key "w" 'my:weblio selected-keymap)
-    (bind-key "p" 'my:postal selected-keymap)
     (bind-key "t" 'google-translate-auto selected-keymap)
-    (bind-key "m" 'my:g-map selected-keymap)
     (bind-key "g" 'my:google selected-keymap)
     (bind-key "l" 'counsel-selected selected-keymap)
     (bind-key "q" 'selected-off selected-keymap))
@@ -70,27 +67,15 @@
     (interactive (list (my:get-region nil)))
     (browse-url (format "https://eow.alc.co.jp/%s/UTF-8/"
 			(upcase (url-hexify-string str)))))
-  ;; Postal code
-  (defun my:postal (str)
-    (interactive (list (my:get-region nil)))
-    (browse-url (format "https://postcode.goo.ne.jp/search/q/%s"
-			(upcase (url-hexify-string str)))))
   ;; Google
   (defun my:google (str)
     (interactive (list (my:get-region nil)))
     (browse-url (format "https://www.google.com/search?hl=ja&q=%s"
 			(upcase (url-hexify-string str)))))
-  ;; Google map
-  (defun my:g-map (str)
-    (interactive (list (my:get-region nil)))
-    (browse-url (format "https://maps.google.co.jp/maps?hl=ja&q=%s"
-			(upcase (url-hexify-string str)))))
   ;; common function
   (defun my:get-region (r)
     "Get search word from region."
-    (buffer-substring-no-properties (region-beginning) (region-end)))
-  )
-
+    (buffer-substring-no-properties (region-beginning) (region-end))))
 
 
 ;; Local Variables:

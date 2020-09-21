066ae8997c97bdbbca02981cdc7e5f425d9e2f99
minorugh <minorugh@gmail.com>
2020-09-21 12:32:11 +0900

    Fix viewer

--- inits/10_view-mode.el
+++ inits/10_view-mode.el
@@ -10,7 +10,6 @@
   :init
   (when (require 'viewer nil t)
 	(viewer-change-modeline-color-setup)
-	(setq viewer-modeline-color-unwritable "orange")
 	(setq viewer-modeline-color-view "#852941")))
 
 

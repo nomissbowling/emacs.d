dde6d98556b8a98e9163458544a725256c722ae6
minorugh <minorugh@gmail.com>
2020-09-22 09:43:44 +0900

    Fix key bind

--- inits/80_easy-hugo.el
+++ inits/80_easy-hugo.el
@@ -11,8 +11,6 @@
   (bind-key "C-x p" 'easy-hugo-preview)
   (bind-key "C-x P" 'easy-hugo-publish)
   (bind-key "C-c d" 'inseart-date)
-  ;; (bind-key "s-t" 'easy-hugo-complete-tags)
-  ;; (bind-key "s-c" 'easy-hugo-complete-categories)
   (with-eval-after-load 'easy-hugo
     (bind-key [tab] 'easy-hugo-no-help easy-hugo-mode-map)
     (bind-key "v" 'easy-hugo-view-other-window easy-hugo-mode-map)
@@ -29,46 +27,46 @@
   (leaf toml-mode :ensure t)
   ;; Main blog (=blog1)
   (setq easy-hugo-basedir "~/Dropbox/web/wegh/topics/"
-	easy-hugo-url "https://topics.wegh.net"
-	easy-hugo-sshdomain "xsrv"
-	easy-hugo-root "/home/minorugh/wegh.net/public_html/topics/"
-	easy-hugo-previewtime "300")
+		easy-hugo-url "https://topics.wegh.net"
+		easy-hugo-sshdomain "xsrv"
+		easy-hugo-root "/home/minorugh/wegh.net/public_html/topics/"
+		easy-hugo-previewtime "300")
   ;; Bloglist
   (setq easy-hugo-bloglist
-	;; blog2 setting
-	'(((easy-hugo-basedir . "~/Dropbox/web/wegh/snap/")
-	   (easy-hugo-url . "https://snap.wegh.net")
-	   (easy-hugo-sshdomain . "xsrv")
-	   (easy-hugo-root . "/home/minorugh/wegh.net/public_html/snap/"))
-	  ;; blog3 setting
-	  ((easy-hugo-basedir . "~/Dropbox/web/wegh/blog/")
-	   (easy-hugo-url . "https://blog.wegh.net")
-	   (easy-hugo-sshdomain . "xsrv")
-	   (easy-hugo-root . "/home/minorugh/wegh.net/public_html/blog/"))
-	  ;; blog4 setting
-	  ((easy-hugo-basedir . "~/Dropbox/web/wegh/essay/")
-	   (easy-hugo-url . "https://essay.wegh.net")
-	   (easy-hugo-sshdomain . "xsrv")
-	   (easy-hugo-root . "/home/minorugh/wegh.net/public_html/essay/"))
-	  ;; blog5 setting
-	  ((easy-hugo-basedir . "~/Dropbox/web/wegh/bible/")
-	   (easy-hugo-url . "https://bible.wegh.net")
-	   (easy-hugo-sshdomain . "xsrv")
-	   (easy-hugo-root . "/home/minorugh/wegh.net/public_html/bible/"))
-	  ;; blog6 setting
-	  ((easy-hugo-basedir . "~/Dropbox/web/wegh/tube/")
-	   (easy-hugo-url . "https://tube.wegh.net")
-	   (easy-hugo-sshdomain . "xsrv")
-	   (easy-hugo-root . "/home/minorugh/wegh.net/public_html/tube/"))
-	  ;; blog7 setting
-	  ((easy-hugo-basedir . "~/Dropbox/web/wegh/ryo/")
-	   (easy-hugo-url . "https://ryo.wegh.net")
-	   (easy-hugo-sshdomain . "xsrv")
-	   (easy-hugo-root . "/home/minorugh/wegh.net/public_html/ryo/"))))
+		;; blog2 setting
+		'(((easy-hugo-basedir . "~/Dropbox/web/wegh/snap/")
+		   (easy-hugo-url . "https://snap.wegh.net")
+		   (easy-hugo-sshdomain . "xsrv")
+		   (easy-hugo-root . "/home/minorugh/wegh.net/public_html/snap/"))
+		  ;; blog3 setting
+		  ((easy-hugo-basedir . "~/Dropbox/web/wegh/blog/")
+		   (easy-hugo-url . "https://blog.wegh.net")
+		   (easy-hugo-sshdomain . "xsrv")
+		   (easy-hugo-root . "/home/minorugh/wegh.net/public_html/blog/"))
+		  ;; blog4 setting
+		  ((easy-hugo-basedir . "~/Dropbox/web/wegh/essay/")
+		   (easy-hugo-url . "https://essay.wegh.net")
+		   (easy-hugo-sshdomain . "xsrv")
+		   (easy-hugo-root . "/home/minorugh/wegh.net/public_html/essay/"))
+		  ;; blog5 setting
+		  ((easy-hugo-basedir . "~/Dropbox/web/wegh/bible/")
+		   (easy-hugo-url . "https://bible.wegh.net")
+		   (easy-hugo-sshdomain . "xsrv")
+		   (easy-hugo-root . "/home/minorugh/wegh.net/public_html/bible/"))
+		  ;; blog6 setting
+		  ((easy-hugo-basedir . "~/Dropbox/web/wegh/tube/")
+		   (easy-hugo-url . "https://tube.wegh.net")
+		   (easy-hugo-sshdomain . "xsrv")
+		   (easy-hugo-root . "/home/minorugh/wegh.net/public_html/tube/"))
+		  ;; blog7 setting
+		  ((easy-hugo-basedir . "~/Dropbox/web/wegh/ryo/")
+		   (easy-hugo-url . "https://ryo.wegh.net")
+		   (easy-hugo-sshdomain . "xsrv")
+		   (easy-hugo-root . "/home/minorugh/wegh.net/public_html/ryo/"))))
 
   ;; Customize for my help menu
   (setq easy-hugo-help-line 5
-	easy-hugo-help "  n .. New blog post    r .. Rename file     p .. Preview          g .. Refresh
+		easy-hugo-help "  n .. New blog post    r .. Rename file     p .. Preview          g .. Refresh
   d .. Delete post      a .. Search blog ag  P .. Publish clever   e .. Edit easy-hugo
   u .. Sort publish     s .. Sort time       < .. Previous blog    > .. Next bloge
   T .. publish timer    m .. Magit status    c .. Open config      f .. Open file

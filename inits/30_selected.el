e6c5a0c9595528a6732ec8ec546a7070391957d2
minorugh <minorugh@gmail.com>
2020-09-22 10:01:28 +0900

    Refactor

--- inits/30_selected.el
+++ inits/30_selected.el
@@ -23,61 +23,57 @@
     (bind-key "q" 'selected-off selected-keymap))
   :global-minor-mode selected-global-mode
   :init
-  (leaf counsel-selected :el-get takaxp/counsel-selected))
+  (leaf counsel-selected :el-get takaxp/counsel-selected)
 
-
-(leaf  control-mozc-when-seleceted
-  :init
+  :preface
+  ;; Control mozc when seleceted
   (defun my-activate-selected ()
-    (selected-global-mode 1)
-    (selected--on)
-    (remove-hook 'activate-mark-hook #'my-activate-selected))
+	(selected-global-mode 1)
+	(selected--on)
+	(remove-hook 'activate-mark-hook #'my-activate-selected))
   (add-hook 'activate-mark-hook #'my-activate-selected)
 
   (defun my:ime-on ()
-    (interactive)
-    (when (null current-input-method) (toggle-input-method)))
+	(interactive)
+	(when (null current-input-method) (toggle-input-method)))
   (defun my:ime-off ()
-    (interactive)
-    (inactivate-input-method))
+	(interactive)
+	(inactivate-input-method))
 
   (defvar my:ime-flag nil)
   (add-hook
    'activate-mark-hook
    #'(lambda ()
-       (setq my:ime-flag current-input-method) (my:ime-off)))
+	   (setq my:ime-flag current-input-method) (my:ime-off)))
   (add-hook
    'deactivate-mark-hook
    #'(lambda ()
-       (unless (null my:ime-flag) (my:ime-on)))))
+	   (unless (null my:ime-flag) (my:ime-on))))
 
-
-(leaf user-functions-for-selected
-  :init
-  ;; Kojien
+  ;; User-functions-for-selected
   (defun my:koujien (str)
-    (interactive (list (my:get-region nil)))
-    (browse-url (format "https://sakura-paris.org/dict/広辞苑/prefix/%s"
-			(upcase (url-hexify-string str)))))
-  ;; Weblio
+	(interactive (list (my:get-region nil)))
+	(browse-url (format "https://sakura-paris.org/dict/広辞苑/prefix/%s"
+						(upcase (url-hexify-string str)))))
+
   (defun my:weblio (str)
-    (interactive (list (my:get-region nil)))
-    (browse-url (format "https://www.weblio.jp/content/%s"
-			(upcase (url-hexify-string str)))))
-  ;; Eijiro
+	(interactive (list (my:get-region nil)))
+	(browse-url (format "https://www.weblio.jp/content/%s"
+						(upcase (url-hexify-string str)))))
+
   (defun my:eijiro (str)
-    (interactive (list (my:get-region nil)))
-    (browse-url (format "https://eow.alc.co.jp/%s/UTF-8/"
-			(upcase (url-hexify-string str)))))
-  ;; Google
+	(interactive (list (my:get-region nil)))
+	(browse-url (format "https://eow.alc.co.jp/%s/UTF-8/"
+						(upcase (url-hexify-string str)))))
+
   (defun my:google (str)
-    (interactive (list (my:get-region nil)))
-    (browse-url (format "https://www.google.com/search?hl=ja&q=%s"
-			(upcase (url-hexify-string str)))))
-  ;; common function
+	(interactive (list (my:get-region nil)))
+	(browse-url (format "https://www.google.com/search?hl=ja&q=%s"
+						(upcase (url-hexify-string str)))))
+
   (defun my:get-region (r)
-    "Get search word from region."
-    (buffer-substring-no-properties (region-beginning) (region-end))))
+	"Get search word from region."
+	(buffer-substring-no-properties (region-beginning) (region-end))))
 
 
 ;; Local Variables:

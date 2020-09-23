db9f278d3bd6add031d84a007f8e231244196536
minorugh <minorugh@gmail.com>
2020-09-23 23:45:47 +0900

    Refactor

--- inits/80_twitter.el
+++ inits/80_twitter.el
@@ -44,6 +44,15 @@
   :ensure t
   :require t
   :config
+  (bind-key "F" 'twittering-favorite twittering-mode-map)
+  (bind-key "r" 'twittering-enter twittering-mode-map)
+  (bind-key "Q" 'twittering-organic-retweet twittering-mode-map)
+  (bind-key "T" 'twittering-native-retweet twittering-mode-map)
+  (bind-key "M" 'twittering-direct-message twittering-mode-map)
+  (bind-key "." 'twittering-current-timeline twittering-mode-map) ;; refresh
+  (bind-key "f" 'twittering-kill-and-switch-to-next-timeline twittering-mode-map)
+  (bind-key "b" 'twittering-kill-and-switch-to-previous-timeline twittering-mode-map)
+  :init
   (setq twittering-use-master-password t)
   (setq twittering-use-ssl t)
   (setq twittering-timer-interval 40)
@@ -53,42 +62,34 @@
   (setq twittering-number-of-tweets-on-retrieval 50)  ;; Tweet display count
   (setq twittering-scroll-mode nil)
   (setq twittering-pop-to-buffer-function 'pop-to-buffer)
-
-  (bind-key "F" 'twittering-favorite twittering-mode-map)
-  (bind-key "r" 'twittering-enter twittering-mode-map)
-  (bind-key "Q" 'twittering-organic-retweet twittering-mode-map)
-  (bind-key "T" 'twittering-native-retweet twittering-mode-map)
-  (bind-key "M" 'twittering-direct-message twittering-mode-map)
-  (bind-key "." 'twittering-current-timeline twittering-mode-map) ;; refresh
-  (bind-key "f" 'twittering-kill-and-switch-to-next-timeline twittering-mode-map)
-  (bind-key "b" 'twittering-kill-and-switch-to-previous-timeline twittering-mode-map)
-
   ;; RT format
   (setq twittering-retweet-format '(nil _ " %u QT @%s: %t"))
-
   ;; TL flows downward
   (setq twittering-reverse-mode t)
-
   ;; Display format
   (setq twittering-status-format "%i%FACE[twittering-mode-name-face]{%s(%S) %p }%FACE[twittering-mode-reply-face]{%r%R}\n%FACE[twittering-mode-text-face]{%t}\n%FACE[twittering-mode-hide-face]{%C{%m/%d %H:%M:%S}(%@)}%FACE[twittering-mode-hide-face]{  from %f%L}%FACE[twittering-mode-sepa-face]{\n\n----------------------------------------------------------\n}")
-
   ;; URL shortening service to j.mp
   (setq twittering-tinyurl-service 'j.mp)
   (setq twittering-bitly-login "minorugh")
   (setq twittering-bitly-api-key "R_f0b3887698d4d171004f55af6e6a199e")
 
+  :preface
   ;; look for name
   (defface twittering-mode-name-face
     '((t (:foreground "#81a2be"))) nil)
+
   ;; look for tweet characters
   (defface twittering-mode-text-face
     '((t (:foreground "#ffffff"))) nil)
+
   ;; look for date and time
   (defface twittering-mode-hide-face
     '((t (:foreground "#f0c674"))) nil)
+
   ;; look for in reply to
   (defface twittering-mode-reply-face
     '((t (:foreground "#b5bd68"))) nil)
+
   ;; look for break
   (defface twittering-mode-sepa-face
     '((t (:foreground "#969896"))) nil)

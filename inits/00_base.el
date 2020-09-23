60cc666af82cc53ae252f4a263c1565b7291d25e
minorugh <minorugh@gmail.com>
2020-09-23 21:38:30 +0900

    Refactor

--- inits/00_base.el
+++ inits/00_base.el
@@ -9,20 +9,28 @@
   :config
   ;; Save hist
   (savehist-mode)
+
   ;;Save plae
   (save-place-mode)
+
   ;; Automatic reloading of changed files
   (global-auto-revert-mode)
+
   ;; Do not blink the cursor
   (blink-cursor-mode)
+
   ;; Window configuration undo/redo
   (winner-mode)
+
   ;; font-lock
   (global-font-lock-mode)
+
   ;; word wrapping is used
   (global-visual-line-mode)
+
   ;; fringe-mode for right- only
   (fringe-mode (cons 0 nil))
+
   ;; frame
   (set-frame-parameter nil 'fullscreen 'maximized)
   (scroll-bar-mode 0)
@@ -30,53 +38,73 @@
   (menu-bar-mode 0)
   (setq inhibit-splash-screen t)
   (setq inhibit-startup-message t)
+
   ;; All warning sounds and flash are invalid (note that the warning sound does not sound completely)
   (setq ring-bell-function 'ignore)
+
   ;; unable right-to-left language reordering
   (setq-default bidi-display-reordering nil)
+
   ;; Point keeps its screen position when scroll
   (setq scroll-preserve-screen-position :always)
+
   ;; Turn Off warning sound screen flash
   (setq visible-bell nil)
+
   ;; Do not change the position of the cursor when scrolling pages
   (setq scroll-preserve-screen-position t)
+
   ;; Suppress warnings for 'ad-handle-definition:'
   (setq ad-redefinition-action 'accept)
+
   ;; Do not distinguish uppercase and lowercase letters on completion
   (setq completion-ignore-case t)
   (setq read-file-name-completion-ignore-case t)
+
   ;; Copy with mouse drag
   (setq mouse-drag-copy-region t)
+
   ;; Do not make a backup filie like *.~
   (setq make-backup-files nil)
+
   ;; Do not use auto save
   (setq auto-save-default nil)
+
   ;; Do not create lock file
   (setq create-lockfiles nil)
+
   ;; Open symbolic link directly
   (setq vc-follow-symlinks t)
+
   ;; Customize tab width
   (setq-default tab-width 4)
+
   :init
   ;; Save the file specified code with basic utf-8 if it exists
   (set-language-environment "Japanese")
   (prefer-coding-system 'utf-8)
+
   ;; font
   (add-to-list 'default-frame-alist '(font . "Cica-18"))
   ;; for sub-machine
   (when (string-match "x250" (shell-command-to-string "uname -n"))
     (add-to-list 'default-frame-alist '(font . "Cica-15")))
+
   ;; Exit Emacs with M-x exitle
   (defalias 'exit 'save-buffers-kill-emacs)
+
   ;; Input yes or no to y or n (even SPC OK instead of y)
   (defalias 'yes-or-no-p 'y-or-n-p)
+
   ;; Set transparency (active inactive)
   (add-to-list 'default-frame-alist '(alpha . (1.0 0.9)))
+
   ;; Set buffer that can not be killed
   (with-current-buffer "*scratch*"
     (emacs-lock-mode 'kill))
   (with-current-buffer "*Messages*"
     (emacs-lock-mode 'kill))
+
   ;; Hack emacs-init-time
   (with-eval-after-load "time"
     (defun ad:emacs-init-time ()

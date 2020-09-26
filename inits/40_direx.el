bc6ff04d23ac1c728089e1fcf4d62e6177f0ebb4
minorugh <minorugh@gmail.com>
2020-09-26 23:35:35 +0900

    Refactor

--- inits/40_direx.el
+++ inits/40_direx.el
@@ -9,30 +9,30 @@
   :bind ("<f10>" . direx:jump-to-project-directory)
   :config
   (with-eval-after-load 'direx
-    (bind-key "o" 'direx:open-file direx:direx-mode-map)
-    (bind-key "SPC" 'direx:find-item-other-window direx:direx-mode-map)
-    (bind-key "<f10>" 'quit-window direx:direx-mode-map))
+	(bind-key "o" 'direx:open-file direx:direx-mode-map)
+	(bind-key "SPC" 'direx:find-item-other-window direx:direx-mode-map)
+	(bind-key "<f10>" 'quit-window direx:direx-mode-map))
   :init
   (setq direx:leaf-icon "  " direx:open-icon "📂" direx:closed-icon "📁")
   (push '(direx:direx-mode :position left :width 25 :dedicated t)
 		popwin:special-display-config)
   :preface
   (defun direx:jump-to-project-directory ()
-    "If in project, launch direx-project otherwise start direx."
-    (interactive)
-    (let ((result (ignore-errors (direx-project:jump-to-project-root-other-window) t)))
-      (unless result (direx:jump-to-directory-other-window))))
+	"If in project, launch direx-project otherwise start direx."
+	(interactive)
+	(let ((result (ignore-errors (direx-project:jump-to-project-root-other-window) t)))
+	  (unless result (direx:jump-to-directory-other-window))))
 
   (defun direx:open-file ()
-    "In direx, open the file in associated application."
-    (interactive)
-    (let* ((item (direx:item-at-point!))
+	"In direx, open the file in associated application."
+	(interactive)
+	(let* ((item (direx:item-at-point!))
 		   (file (direx:item-tree item))
 		   (file-full-name (direx:file-full-name file)))
-      (unless (getenv "WSLENV")
+	  (unless (getenv "WSLENV")
 		(call-process "xdg-open" nil 0 nil file-full-name))
-      ;; use wsl-utils:https://github.com/smzht/wsl-utils
-      (when (getenv "WSLENV")
+	  ;; use wsl-utils:https://github.com/smzht/wsl-utils
+	  (when (getenv "WSLENV")
 		(call-process "wslstart" nil 0 nil file-full-name)))))
 
 

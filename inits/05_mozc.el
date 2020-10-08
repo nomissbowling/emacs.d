068b3942282117da12e8694f3cd76ab37240d6ac
minoru <minoru@e590.minorugh.com>
2020-10-08 17:50:45 +0900

    Fix key bind

--- inits/05_mozc.el
+++ inits/05_mozc.el
@@ -6,8 +6,11 @@
 
 (leaf mozc
   :ensure t
-  :bind* ("<hiragana-katakana>" . toggle-input-method)
   :config
+  (unless (getenv "WSLENV")
+	(bind-key* "<hiragana-katakana>" 'toggle-input-method))
+  (when (getenv "WSLENV")
+	(bind-key* "<henkan>" 'toggle-input-method))
   (setq default-input-method "japanese-mozc"
 		mozc-helper-program-name "mozc_emacs_helper"
 		mozc-leim-title "♡かな")
@@ -18,22 +21,22 @@
   (define-key mozc-mode-map "?" '(lambda () (interactive) (mozc-insert-str "？")))
   (define-key mozc-mode-map "!" '(lambda () (interactive) (mozc-insert-str "！")))
   (defun mozc-insert-str (str)
-    "If punctuation marks, immediately confirm."
-    (mozc-handle-event 'enter)
-    (toggle-input-method)
-    (insert str)
-    (toggle-input-method))
+	"If punctuation marks, immediately confirm."
+	(mozc-handle-event 'enter)
+	(toggle-input-method)
+	(insert str)
+	(toggle-input-method))
 
   ;; Key-chord measures
   (defadvice toggle-input-method (around toggle-input-method-around activate)
-    "Input method function in key-chord.el not to be nil."
-    (let ((input-method-function-save input-method-function))
-      ad-do-it
-      (setq input-method-function input-method-function-save)))
+	"Input method function in key-chord.el not to be nil."
+	(let ((input-method-function-save input-method-function))
+	  ad-do-it
+	  (setq input-method-function input-method-function-save)))
 
   :init
   (leaf mozc-cursor-color
-    :el-get iRi-E/mozc-el-extensions
+	:el-get iRi-E/mozc-el-extensions
 	:require t
 	:config
 	(setq mozc-cursor-color-alist
@@ -43,8 +46,8 @@
 
   (leaf mozc-cand-posframe :ensure t
 	:require t
-    :config
-    (setq mozc-candidate-style 'posframe)))
+	:config
+	(setq mozc-candidate-style 'posframe)))
 
 
 (leaf mozc-tool-setting

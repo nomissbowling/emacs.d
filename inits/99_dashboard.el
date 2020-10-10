a56fd05e7f3c655342496b6c1a3978b0c0753af4
minoru <minoru@e590.minorugh.com>
2020-10-10 15:55:24 +0900

    Fix menu

--- inits/99_dashboard.el
+++ inits/99_dashboard.el
@@ -59,7 +59,7 @@
 	(interactive)
 	(insert (if (display-graphic-p)
 				(all-the-icons-faicon "google" :height 1.2 :v-adjust -0.05 :face 'dashboard-heading) " "))
-	(insert "   📰 News: (n)   📝 Keep: (k)    Mail: (m)    Twitter: (t)    Pocket: (p)    Slack: (s)    GH: (h) "))
+	(insert "   📅 Calendar: (c)   📰 News: (n)   📝 Keep: (k)    Mail: (m)    Twitter: (t)    Pocket: (p)    Slack: (s)    GH: (h) "))
 
   (defvar dashboard-recover-layout-p nil
 	"Wether recovers the layout.")

37a2290944cb58c55b09980716cc3281e87ed709
minoru <minoru@e590.minorugh.com>
2020-10-11 05:47:44 +0900

    Fix

--- inits/00_base.el
+++ inits/00_base.el
@@ -42,18 +42,18 @@
   (setq-default tab-width 4)
 
   ;; Hack emacs-init-time
-  (with-eval-after-load "time"
-  	(defun ad:emacs-init-time ()
-  	  "Return a string giving the duration of the Emacs initialization."
-  	  (interactive)
-  	  (let ((str
-  			 (format "%.3f seconds"
-  					 (float-time
-  					  (time-subtract after-init-time before-init-time)))))
-  		(if (called-interactively-p 'interactive)
-  			(message "%s" str)
-  		  str)))
-  	(advice-add 'emacs-init-time :override #'ad:emacs-init-time))
+  ;; (with-eval-after-load "time"
+  ;; 	(defun ad:emacs-init-time ()
+  ;; 	  "Return a string giving the duration of the Emacs initialization."
+  ;; 	  (interactive)
+  ;; 	  (let ((str
+  ;; 			 (format "%.3f seconds"
+  ;; 					 (float-time
+  ;; 					  (time-subtract after-init-time before-init-time)))))
+  ;; 		(if (called-interactively-p 'interactive)
+  ;; 			(message "%s" str)
+  ;; 		  str)))
+  ;; 	(advice-add 'emacs-init-time :override #'ad:emacs-init-time))
 
   ;; Load user defined
   (add-to-list 'load-path "~/Dropbox/emacs.d/elisp")
